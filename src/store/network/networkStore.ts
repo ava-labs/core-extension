c849d43fc324abc5783c2ac9040e839b6f5f4841
paOol <paoolkim@gmail.com>
2021-05-07 18:30:07 -0700

    network store

---
+++ src/store/network/networkStore.ts
@@ -0,0 +1,24 @@
+import { makeAutoObservable } from 'mobx';
+import { persistStore } from '@src/utils/mobx';
+import { setNetwork, NetworkConstants } from 'avalanche-wallet-sdk';
+
+const testnet = NetworkConstants.TestnetConfig;
+const mainnet = NetworkConstants.MainnetConfig;
+
+type Network = string;
+
+class NetworkStore {
+  network: Network = 'testnet';
+
+  constructor() {
+    makeAutoObservable(this);
+    persistStore(this, ['network'], 'NetworkStore');
+  }
+
+  changeNetwork(network) {
+    const net = network === 'testnet' ? testnet : mainnet;
+    setNetwork(net);
+  }
+}
+
+export default NetworkStore;
--- src/store/store.ts
+++ src/store/store.ts
@@ -3,12 +3,14 @@ import AssetsStore from '@src/store/assets/assetsStore';
 import ThemeStore from '@src/store/theme/themeStore';
 import ExtensionStore from '@src/store/extension/extensionStore';
 import OnboardStore from '@src/store/onboard/onboardStore';
+import NetworkStore from '@src/store/network/networkStore';
 
 export interface CombinedStores {
   asssetsStore: AssetsStore;
   themeStore: ThemeStore;
   extensionStore: ExtensionStore;
   onboardStore: OnboardStore;
+  networkStore: NetworkStore;
 }
 
 export const store: CombinedStores = {
@@ -16,6 +18,7 @@ export const store: CombinedStores = {
   themeStore: new ThemeStore(),
   extensionStore: new ExtensionStore(),
   onboardStore: new OnboardStore(),
+  networkStore: new NetworkStore(),
 };
 
 export const StoreContext = createContext(store);
