44ca4ff945228976232780c7601e80c0c49c5ceb
paOol <paoolkim@gmail.com>
2021-05-19 22:54:53 -0700

    wait function

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -2,6 +2,13 @@ export const isInArray = (value: string, array: any[]): boolean => {
   return array.indexOf(value) > -1;
 };
 
+export const wait = (waitTime: number) => {
+  return new Promise<void>((resolve) => {
+    setTimeout(() => resolve(), waitTime);
+  });
+};
+
 export default {
   isInArray,
+  wait,
 };
