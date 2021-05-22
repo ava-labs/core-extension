c599c66bd45c5fbadaf3d7cb53e913a6ea82071d
paOol <paoolkim@gmail.com>
2021-05-21 19:49:11 -0700

    deposit page XPC addresses

--- src/pages/Deposit/Deposit.tsx
+++ src/pages/Deposit/Deposit.tsx
@@ -12,16 +12,50 @@ import { truncateAddress } from '@src/utils/addressUtils';
 
 export const Deposit = () => {
   const [isCopied, setIsCopied] = useState(false);
+  const [chain, setChain] = useState('C');
+
   const { walletStore } = useStore();
 
-  const truncatedAddress = truncateAddress(walletStore.addrC);
+  const getAddress = () => {
+    if (chain === 'C') {
+      return walletStore.addrC;
+    } else if (chain === 'X') {
+      return walletStore.addrX;
+    } else if (chain === 'P') {
+      return walletStore.addrP;
+    }
+    return walletStore.addrC;
+  };
+
+  const truncatedAddress = truncateAddress(getAddress());
   return (
     <Layout>
       <Wrapper>
         <QR>
-          <h1>Deposit AVAX</h1>
+          <h1>Select Chain to 'Receive' your coin</h1>
+          <div className="selector">
+            <span
+              className={chain === 'X' ? 'active' : ''}
+              onClick={() => setChain('X')}
+            >
+              X-Chain
+            </span>
+            <span
+              className={chain === 'P' ? 'active' : ''}
+              onClick={() => setChain('P')}
+            >
+              P-Chain
+            </span>
+            <span
+              className={chain === 'C' ? 'active' : ''}
+              onClick={() => setChain('C')}
+            >
+              C-Chain
+            </span>
+          </div>
+
           <div className="qr">
-            <QRCode value={walletStore.addrC} style={{ width: '100px' }} />
+            <QRCode value={getAddress()} style={{ width: '100px' }} />
           </div>
 
           <div className="address">
@@ -29,14 +63,14 @@ export const Deposit = () => {
             <button
               onClick={() => {
                 setIsCopied(true);
-                navigator.clipboard.writeText(walletStore.addrC);
+                navigator.clipboard.writeText(getAddress());
               }}
             >
               {isCopied ? 'Copied!' : 'Copy'}
             </button>
           </div>
           <div className="disclaimer">
-            This address can only be used to receive AVAX on the __ Chain.
+            This address can only be used to receive AVAX on the {chain} Chain.
           </div>
 
           <Link to="/wallet">
@@ -53,6 +87,27 @@ export const Wrapper = styled.div`
 `;
 
 export const QR = styled.div`
+  .selector {
+    display: inline-flex;
+    justify-content: center;
+    text-align: center;
+    width: 100%;
+    margin-bottom: 1rem;
+    span {
+      width: 75px;
+      height: 26px;
+      align-items: center;
+      display: flex;
+      justify-content: center;
+      font-size: 11px;
+      letter-spacing: -2%;
+      margin: auto 0.4rem;
+    }
+    .active {
+      background: #2d333a;
+      border-radius: 50px;
+    }
+  }
   h1 {
     text-align: center;
     font-size: 1.6rem;
@@ -63,11 +118,10 @@ export const QR = styled.div`
   }
   .address {
     font-size: 1.2rem;
-    background: grey;
     padding: 1rem;
-    margin: 2rem auto;
+    margin: auto 2rem auto;
     display: flex;
-    justify-content: space-between;
+    justify-content: center;
     align-items: center;
   }
 `;
