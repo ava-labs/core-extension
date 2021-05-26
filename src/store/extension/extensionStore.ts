517988ee27adc2a4c67c6e764e2912beb1b96ce3
paOol <paoolkim@gmail.com>
2021-05-25 21:39:42 -0700

    timeout store

--- src/store/extension/extensionStore.ts
+++ src/store/extension/extensionStore.ts
@@ -1,14 +1,47 @@
 import { action, makeAutoObservable, observable } from 'mobx';
 import extension from 'extensionizer';
+import { persistStore } from '@src/utils/mobx';
 
 class ExtensionStore {
-  isDarkMode: boolean = false;
+  isUnlocked: boolean = true;
+  timer: any = null;
+  timeoutMinutes: number = 0;
 
   constructor() {
     makeAutoObservable(this);
+    persistStore(this, ['isUnlocked'], 'ExtensionStore');
   }
   reload() {
     extension.runtime.reload();
+  }
+
+  setLastActiveTime() {
+    this.resetTimer();
+  }
+
+  setInactiveTimeout(timeoutMinutes) {
+    this.timeoutMinutes = timeoutMinutes;
+
+    this.resetTimer();
+  }
+
+  resetTimer() {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+
+    if (!this.timeoutMinutes) {
+      return;
+    }
+
+    this.timer = setTimeout(
+      () => this.setLocked(),
+      this.timeoutMinutes * 60 * 1000
+    );
+  }
+
+  setLocked(): void {
+    this.isUnlocked = false;
   }
 
   openWindow(url: string) {
