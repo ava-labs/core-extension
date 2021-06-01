dd9307463a167388e9a21598ce69130903d9dd73
dfenstermaker <dannyfenstermaker@gmail.com>
2021-06-01 17:51:00 -0600

    almost kind of working

--- src/avalancheweb3.ts
+++ src/avalancheweb3.ts
@@ -1,25 +1,45 @@
-interface Window {
-  testproperty: any;
-  avalanche: any;
+import extension from "extensionizer";
+import PortStream from "extension-port-stream";
+
+declare global {
+  interface Window {
+    avalanche: any;
+  }
 }
 
 class Avalanche {
   constructor() {}
 
   test() {
-    console.log('line 5');
-    return 'test';
+    console.log("line 5");
+    return "test";
   }
 
   getAddress() {
-    return 'asdfasdfasasdf';
+    return "asdfasdfasasdf";
   }
 }
 window.avalanche = new Avalanche();
 const asdf = () => {
   // alert('asdf');
-  console.log('avalanche', window.avalanche);
+  console.log("avalanche", window.avalanche);
+  // identify window type (popup, notification)
+  // const windowType = getEnvironmentType();
 
+  // // setup stream to background
+  // const extensionPort = extension.runtime.connect({ name: windowType });
+  const extensionPort = extension.runtime.connect({ name: "background" });
+  const connectionStream = new PortStream(extensionPort);
+  console.log("stream: ", { extensionPort, connectionStream });
+
+  connectionStream.addListener("ev", (...args) => {
+    console.log("this is from background: ", args);
+  });
+
+  setInterval(() => {
+    console.log("emiting");
+    connectionStream.emit("this is a message from stream");
+  }, 20000);
   window.avalanche.test();
 };
 
--- src/backgroundPage.ts
+++ src/backgroundPage.ts
@@ -1,11 +1,18 @@
-import { browser } from 'webextension-polyfill-ts';
-import extensionizer from 'extensionizer';
+import { browser } from "webextension-polyfill-ts";
+import PortStream from "extension-port-stream";
 
+browser.runtime.onConnect.addListener((connection) => {
+  console.log("on connect attempt: ", connection);
+  // new PortStream(connection).emit("ev", "Back at you :)");
+  connection.postMessage("back to ya :)");
+});
 // Listen for messages sent from other parts of the extension
-browser.runtime.onMessage.addListener((request: { popupMounted: boolean }) => {
+browser.runtime.onMessage.addListener((...args) => {
+  // browser.runtime.onMessage.addListener((request: { popupMounted: boolean }) => {
   // Log statement if request.popupMounted is true
   // NOTE: this request is sent in `popup/component.tsx`
-  if (request.popupMounted) {
-    console.log('backgroundPage notified that Popup.tsx has mounted.');
-  }
+  console.log("made it", args);
+  // if (request.popupMounted) {
+  //   console.log("backgroundPage notified that Popup.tsx has mounted.");
+  // }
 });
