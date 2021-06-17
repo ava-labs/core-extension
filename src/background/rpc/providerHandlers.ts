c73f2f5d145ad2855c1d41ad9751cdbc1e6e513b
dfenstermaker <dannyfenstermaker@gmail.com>
2021-06-17 14:52:50 -0600

    update architecture docs

--- src/background/rpc/providerHandlers.ts
+++ src/background/rpc/providerHandlers.ts
@@ -1,10 +1,18 @@
 import { JsonRpcRequest } from "./jsonRpcEngine";
-
-const AVALANCHE_AGGREGATOR_NAME = "metamask";
+import { store } from "@src/store/store";
+// const AVALANCHE_AGGREGATOR_NAME = "metamask";
 
 export default {
   async metamask_getProviderState(data) {
-    return { ...data, result: { name: "this came from handler" } };
+    return {
+      ...data,
+      result: {
+        isUnlocked: store.extensionStore.isUnlocked,
+        chainId: store.walletStore.addrC,
+        networkVersion: "avax",
+        accounts: store.walletStore.accounts,
+      },
+    };
   },
   async metamask_sendDomainMetadata(data) {
     return { ...data, result: data.params };
