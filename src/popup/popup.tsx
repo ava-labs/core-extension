85961745ec74c0e49ecc0f21cea798c86ebeb745
paOol <paoolkim@gmail.com>
2021-06-21 16:28:32 -0700

    routes

--- src/popup/popup.tsx
+++ src/popup/popup.tsx
@@ -19,72 +19,75 @@ import { Deposit } from '@src/pages/Deposit';
 import { Send } from '@src/pages/Send';
 import { SendConfirm } from '@src/pages/SendConfirm';
 import { SendSuccess } from '@src/pages/SendSuccess';
+import { SignMessage } from '@src/pages/SignMessage';
 import { AddToken } from '@src/components/AddToken';
 
-export const Popup = observer(
-  (): React.ReactElement => {
-    const { themeStore, networkStore, walletStore } = useStore();
+export const Popup = observer((): React.ReactElement => {
+  const { themeStore, networkStore, walletStore } = useStore();
 
-    // Sends the `popupMounted` event
-    useEffect(() => {
-      networkStore.changeNetwork('testnet');
-      walletStore.MnemonicWallet();
-      browser.runtime.sendMessage({ popupMounted: true });
-    }, []);
+  // Sends the `popupMounted` event
+  useEffect(() => {
+    networkStore.changeNetwork('testnet');
+    walletStore.MnemonicWallet();
+    browser.runtime.sendMessage({ popupMounted: true });
+  }, []);
 
-    return (
-      <ThemeProvider
-        theme={themeStore.isDarkMode ? darkModeTheme : lightModeTheme}
-      >
-        <ExtensionContainer>
-          <Switch>
-            <Route path="/welcome/create">
-              <CreateWallet />
-            </Route>
+  return (
+    <ThemeProvider
+      theme={themeStore.isDarkMode ? darkModeTheme : lightModeTheme}
+    >
+      <ExtensionContainer>
+        <Switch>
+          <Route path="/welcome/create">
+            <CreateWallet />
+          </Route>
 
-            <Route path="/welcome">
-              <Welcome />
-            </Route>
+          <Route path="/welcome">
+            <Welcome />
+          </Route>
 
-            <Route path="/import">
-              <Import />
-            </Route>
+          <Route path="/import">
+            <Import />
+          </Route>
 
-            <Route path="/token/add">
-              <AddToken />
-            </Route>
+          <Route path="/token/add">
+            <AddToken />
+          </Route>
 
-            <Route path="/wallet/overview">
-              <WalletOverview />
-            </Route>
+          <Route path="/wallet/overview">
+            <WalletOverview />
+          </Route>
 
-            <Route path="/wallet">
-              <WalletHome />
-            </Route>
+          <Route path="/wallet">
+            <WalletHome />
+          </Route>
 
-            <Route path="/deposit">
-              <Deposit />
-            </Route>
+          <Route path="/deposit">
+            <Deposit />
+          </Route>
 
-            <Route path="/send/confirm">
-              <SendConfirm />
-            </Route>
+          <Route path="/send/confirm">
+            <SendConfirm />
+          </Route>
 
-            <Route path="/send/success">
-              <SendSuccess />
-            </Route>
+          <Route path="/send/success">
+            <SendSuccess />
+          </Route>
 
-            <Route path="/send">
-              <Send />
-            </Route>
+          <Route path="/send">
+            <Send />
+          </Route>
 
-            <Route path="/">
-              <FirstTimeFlow />
-            </Route>
-          </Switch>
-          <GlobalStyle />
-        </ExtensionContainer>
-      </ThemeProvider>
-    );
-  }
-);
+          <Route path="/sign">
+            <SignMessage />
+          </Route>
+
+          <Route path="/">
+            <FirstTimeFlow />
+          </Route>
+        </Switch>
+        <GlobalStyle />
+      </ExtensionContainer>
+    </ThemeProvider>
+  );
+});
