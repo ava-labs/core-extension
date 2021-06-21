4f4ad7ed8b8538f418da120d8ec0ce127b4cba7d
paOol <paoolkim@gmail.com>
2021-06-21 16:29:44 -0700

    add message types

--- src/store/transaction/types.ts
+++ src/store/transaction/types.ts
@@ -17,3 +17,21 @@ export interface txParams {
   gas?: string;
   gasPrice?: string;
 }
+
+export type MessageType =
+  | 'eth_decrypt'
+  | 'eth_signTypedData'
+  | 'eth_getEncryptionPublicKey'
+  | 'metamask_getProviderState'
+  | 'personal_sign'
+  | 'wallet_watchAsset'
+  | 'metamask_watchAsset';
+
+export interface UnapprovedMessage {
+  id: number | string | void;
+  from: string;
+  msgParams: any;
+  time: number;
+  status: string;
+  type: MessageType;
+}
