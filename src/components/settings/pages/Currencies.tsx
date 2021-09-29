5f4ccfd8fb3aad84a148784986102d35937f19b1
Gergely Lovas <gergely.lovas@avalabs.org>
2021-09-29 12:55:53 +0200

    add currencies page

--- src/components/settings/pages/Currencies.tsx
+++ src/components/settings/pages/Currencies.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   CaretIcon,
+  CheckmarkIcon,
   DropDownMenuItem,
   HorizontalFlex,
   HorizontalSeparator,
@@ -15,72 +16,56 @@ import { resetExtensionState } from '@src/utils/resetExtensionState';
 import { useSettingsContext } from '@src/contexts/SettingsProvider';
 import { SettingsPageProps } from '../models';
 
-export function Currencies({ navigateTo, goBack }: SettingsPageProps) {
+// TODO: replace it with the supported currency list and wite it up to the backend
+const currencies = [
+  { name: 'United States Dollar', symbol: 'USD' },
+  { name: 'Euro', symbol: 'EUR' },
+  { name: 'Australian Dollar', symbol: 'AUD' },
+  { name: 'Canadian Dollar', symbol: 'CAD' },
+  { name: 'Swiss Franc', symbol: 'CHF' },
+  { name: 'Chilean Peso', symbol: 'CLP' },
+  { name: 'Chech Koruna', symbol: 'CZK' },
+  { name: 'Danish Krone', symbol: 'DKK' },
+  { name: 'British Pound Sterling', symbol: 'GBP' },
+  { name: 'Hong Kong Dollar', symbol: 'HKD' },
+  { name: 'Hungarian Forint', symbol: 'HUF' },
+  { name: 'Israeli New Shekel', symbol: 'ILS' },
+  { name: 'Indian Rupee', symbol: 'INR' },
+];
+
+export function Currencies({ goBack }: SettingsPageProps) {
   const theme = useTheme();
-  const { lockWallet } = useSettingsContext();
+  const [selectedCurrency, setSelectedCurrency] = useState('USD');
 
   return (
     <VerticalFlex width="375px" padding="12px 0">
-      <HorizontalFlex grow="1" justify="space-between" padding="12px 24px">
-        <CaretIcon
-          height="20px"
-          direction={IconDirection.LEFT}
-          onClick={goBack}
-        />
+      <HorizontalFlex grow="1" padding="12px 24px">
+        <TextButton onClick={goBack} margin="0 8px 0 0">
+          <CaretIcon
+            height="20px"
+            direction={IconDirection.LEFT}
+            color={theme.colors.text}
+          />
+        </TextButton>
         <Typography size={18} weight={700} height="24px">
           Currencies
         </Typography>
       </HorizontalFlex>
-      <DropDownMenuItem justify="space-between" align="center">
-        <Typography>Currency</Typography>
-        <CaretIcon
-          color={theme.colors.textAccent}
-          height="12px"
-          direction={IconDirection.RIGHT}
-        />
-      </DropDownMenuItem>
-      <DropDownMenuItem justify="space-between" align="center">
-        <Typography>Security &amp; Privacy</Typography>
-        <CaretIcon
-          color={theme.colors.textAccent}
-          height="12px"
-          direction={IconDirection.RIGHT}
-        />
-      </DropDownMenuItem>
-      <DropDownMenuItem justify="space-between" align="center">
-        <Typography>Legal</Typography>
-        <CaretIcon
-          color={theme.colors.textAccent}
-          height="12px"
-          direction={IconDirection.RIGHT}
-        />
-      </DropDownMenuItem>
-      <DropDownMenuItem justify="space-between" align="center">
-        <Typography>Advanced</Typography>
-        <CaretIcon
-          color={theme.colors.textAccent}
-          height="12px"
-          direction={IconDirection.RIGHT}
-        />
-      </DropDownMenuItem>
-      <DropDownMenuItem justify="space-between">
-        <Typography>Hide tokens without balance</Typography>
-      </DropDownMenuItem>
-      <HorizontalSeparator margin="12px 0" />
-      <DropDownMenuItem justify="space-between" onClick={() => lockWallet()}>
-        <Typography>Lock wallet</Typography>
-        <LockIcon color={theme.colors.textAccent} />
-      </DropDownMenuItem>
-      <DropDownMenuItem justify="space-between">
-        <Typography>Version</Typography>
-        <Typography color={theme.colors.textAccent}>0.0.0</Typography>
-      </DropDownMenuItem>
-      <DropDownMenuItem
-        justify="space-between"
-        onClick={() => resetExtensionState()}
-      >
-        <TextButton>Sign out</TextButton>
-      </DropDownMenuItem>
+      {currencies.map((c) => (
+        <DropDownMenuItem
+          key={c.symbol}
+          justify="space-between"
+          align="center"
+          onClick={() => setSelectedCurrency(c.symbol)}
+        >
+          <Typography>
+            {c.name} ({c.symbol})
+          </Typography>
+          {selectedCurrency === c.symbol && (
+            <CheckmarkIcon size="16px" color={theme.colors.primary[400]} />
+          )}
+        </DropDownMenuItem>
+      ))}
     </VerticalFlex>
   );
 }
