c08a19fdecddfabae95fce96e299b6e1758fc543
Gergely Lovas <gergely.lovas@avalabs.org>
2021-10-04 11:07:09 +0200

    scroll to selected account

--- src/components/common/account/AccountDopdownContent.tsx
+++ src/components/common/account/AccountDopdownContent.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import {
   CheckmarkIcon,
   DropDownMenuItem,
@@ -30,6 +30,17 @@ export function AccountDropdownContent() {
   const theme = useTheme();
   const { addresses } = useWalletContext();
   const [selectedAccount, setSelectedAccount] = useState(0);
+  const scrollbarsRef = useRef<Scrollbars>(null);
+  const selectedAccountRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!scrollbarsRef || !selectedAccountRef || !selectedAccount) {
+      return;
+    }
+    if (selectedAccountRef.current?.offsetTop) {
+      scrollbarsRef.current?.scrollTop(selectedAccountRef.current.offsetTop);
+    }
+  }, [selectedAccount, selectedAccountRef, scrollbarsRef]);
 
   const accounts = [
     {
@@ -67,13 +78,17 @@ export function AccountDropdownContent() {
       <Typography size={18} weight={700} padding="12px 24px" height="24px">
         My Accounts
       </Typography>
-      <Scrollbars style={{ flexGrow: 1, maxHeight: 'unset', height: '100%' }}>
+      <Scrollbars
+        style={{ flexGrow: 1, maxHeight: 'unset', height: '100%' }}
+        ref={scrollbarsRef}
+      >
         {accounts.map((account, i) => (
           <DropDownMenuItem
             key={i}
             selected={selectedAccount === i}
             padding="16px 32px"
             onClick={() => setSelectedAccount(i)}
+            ref={selectedAccount === i ? selectedAccountRef : undefined}
           >
             <VerticalFlex width="100%">
               <HorizontalFlex
