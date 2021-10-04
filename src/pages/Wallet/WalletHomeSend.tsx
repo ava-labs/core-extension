2500a9fc7da8fdfd92baa5113f849ba246705419
dfenstermaker <dannyfenstermaker@gmail.com>
2021-10-04 11:30:13 -0600

    fix for network selection token persisting

--- src/pages/Wallet/WalletHomeSend.tsx
+++ src/pages/Wallet/WalletHomeSend.tsx
@@ -1,9 +1,37 @@
 import { VerticalFlex } from '@avalabs/react-components';
-import React from 'react';
+import { AVAX_TOKEN } from '@avalabs/wallet-react-components';
+import { networkUpdatedEventListener } from '@src/background/services/network/events/networkUpdatedEventListener';
+import { useConnectionContext } from '@src/contexts/ConnectionProvider';
+import { useSetTokenInParams } from '@src/hooks/useSetTokenInParams';
+import React, { useEffect } from 'react';
+import { filter, map } from 'rxjs';
+import { TransactionSendType } from '../Send/models';
 import SendFlow from '../Send/SendFlow';
 import { WalletSendToken } from './components/WalletSendToken';
 
 export function WalletHomeSend() {
+  const { events } = useConnectionContext();
+  const setTokenInParams = useSetTokenInParams();
+
+  useEffect(() => {
+    if (!events) {
+      return;
+    }
+    /**
+     * We need to listen for network to update and when it does reset the token
+     * selected back to avax since the previous token may not be on the network
+     * but AVAX always is guaranteed
+     */
+    events()
+      .pipe(
+        filter(networkUpdatedEventListener),
+        map((evt) => evt.value)
+      )
+      .subscribe(() => {
+        setTokenInParams(AVAX_TOKEN.symbol, TransactionSendType.AVAX);
+      });
+  }, []);
+
   return (
     <VerticalFlex>
       <br />
