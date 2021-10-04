169d087a1f98a5e2d53cd7fbe90c40694b1f2912
dfenstermaker <dannyfenstermaker@gmail.com>
2021-10-04 10:15:29 -0600

    fix for incorrect password

--- src/background/services/wallet/storage.ts
+++ src/background/services/wallet/storage.ts
@@ -1,3 +1,4 @@
+import { resolve } from '@src/utils/promiseResolver';
 import {
   getFromStorage,
   removeFromStorage,
@@ -19,9 +20,20 @@ export function saveMnemonicToStorage(mnemonic: string, password: string) {
 }
 
 export async function decryptMnemonicInStorage(password: string) {
-  const cipher = await getMnemonicFromStorage();
-  const bytes = AES.decrypt(cipher, password);
-  return bytes.toString(enc.Utf8);
+  try {
+    /**
+     * For some reason errors are diff depending on the length of the password
+     * how many times its been ran. Not quite sure but we put in EXTRA protection
+     * just in case.
+     */
+    const [cipher, err] = await resolve(getMnemonicFromStorage());
+    if (err) return Promise.reject(err);
+    if (!cipher) return Promise.reject(new Error('password incorrect'));
+    const bytes = AES.decrypt(cipher, password);
+    return bytes.toString(enc.Utf8);
+  } catch (err) {
+    return Promise.reject(err);
+  }
 }
 
 export async function removeWalletFromStorage() {
