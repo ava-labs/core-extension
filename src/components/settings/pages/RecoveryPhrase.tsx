c35efa357077c6edcdf4e5beae5930394f8c8eb5
Andy Bolos <andybolos@gmail.com>
2021-10-20 12:10:30 -0600

    feat(settings): shows recovery phrase
    Adds a password input and show button and textarea for recovery phrase.

--- src/components/settings/pages/RecoveryPhrase.tsx
+++ src/components/settings/pages/RecoveryPhrase.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
-import { VerticalFlex } from '@avalabs/react-components';
+import React, { useState } from 'react';
+import {
+  VerticalFlex,
+  Input,
+  PrimaryButton,
+  Typography,
+  ComponentSize,
+  TextArea,
+} from '@avalabs/react-components';
 import { SettingsPageProps } from '../models';
 import { SettingsHeader } from '../SettingsHeader';
 
 export function RecoveryPhrase({ goBack, navigateTo }: SettingsPageProps) {
+  const [passwordValue, setPasswordValue] = useState('');
+  const [showSeedPhrase, setShowSeedPhrase] = useState(false);
+
+  const handleShowSeedPhrase = () => {
+    // check password against user password
+    // if match -> show seed phrase
+    if (passwordValue === 'test') {
+      setShowSeedPhrase(true);
+    }
+    // else show error?
+  };
+
   return (
     <VerticalFlex width="375px" padding="12px 0">
       <SettingsHeader
@@ -11,6 +30,30 @@ export function RecoveryPhrase({ goBack, navigateTo }: SettingsPageProps) {
         navigateTo={navigateTo}
         title={'Show recovery phrase'}
       />
+      {!showSeedPhrase && (
+        <>
+          <Input
+            label="Password"
+            onChange={(e) => setPasswordValue(e.target.value)}
+            value={passwordValue}
+            placeholder="password"
+            type="password"
+          />
+          <PrimaryButton
+            size={ComponentSize.LARGE}
+            onClick={() => handleShowSeedPhrase()}
+            // disabled={!sendState?.canSubmit}
+            margin="0 0 24px"
+          >
+            <Typography size={14} color="inherit">
+              Show
+            </Typography>
+          </PrimaryButton>
+        </>
+      )}
+      {showSeedPhrase && (
+        <TextArea value={'Seed phrase will go here'} disabled></TextArea>
+      )}
     </VerticalFlex>
   );
 }
