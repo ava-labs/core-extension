76dd3ae40dad106b8beae2532be24775134058ce
Danny Fenstermaker <dannyfenstermaker@gmail.com>
2021-11-18 14:28:05 -0700

    feat(confirm): unknown confirm styles updated (#84)
    * feat(confirm): unknown confirm styles updated

--- src/pages/SignTransaction/UnknownTx.tsx
+++ src/pages/SignTransaction/UnknownTx.tsx
@@ -1,10 +1,18 @@
 import { Typography, VerticalFlex } from '@avalabs/react-components';
+import { TransactionDisplayValues } from '@src/background/services/transactions/models';
 import React from 'react';
+import { AddressPaths } from './components/AddressPaths';
 
-export function UnknownTx() {
+export function UnknownTx({
+  fromAddress,
+  toAddress,
+}: TransactionDisplayValues) {
   return (
-    <VerticalFlex align={'center'}>
-      <Typography>Unknown Tx Type</Typography>
+    <VerticalFlex align={'center'} width="100%">
+      <Typography size={24} weight={700} margin={'8px 0 16px 0'}>
+        Transaction Summary
+      </Typography>
+      <AddressPaths fromAddress={fromAddress} toAddress={toAddress} />
     </VerticalFlex>
   );
 }
