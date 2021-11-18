c0fdb3f5bb69ad87d61e75089fea1e7a3cec1997
Oliver Wang <oliver.wang4@gmail.com>
2021-11-18 13:21:28 -0800

    fix pushing account changed/network changed/wallet lock state to dApp (#82)
    * fix accounts changed events
    
    * add wallet locket event push to dapp
    
    Co-authored-by: Oliver Wang <oliver.wang@avalabs.org>

--- src/background/services/web3/events/unlockStateChangedEvent.ts
+++ src/background/services/web3/events/unlockStateChangedEvent.ts
@@ -1,4 +1,4 @@
-import { firstValueFrom, switchMap } from 'rxjs';
+import { firstValueFrom, map, switchMap, tap } from 'rxjs';
 import { getAccountsFromWallet } from '../../wallet/utils/getAccountsFromWallet';
 import { wallet$ } from '@avalabs/wallet-react-components';
 import { walletLocked$ } from '../../wallet/walletLocked';
@@ -6,20 +6,21 @@ import { Web3Event } from './models';
 
 export function unlockStateChangedEvents() {
   return walletLocked$.pipe(
-    switchMap(async (lockedState) => {
-      const isUnlocked = lockedState?.locked;
-      const wallet = await firstValueFrom(wallet$);
-
-      const accounts =
-        isUnlocked && wallet ? getAccountsFromWallet(wallet) : [];
-
-      return {
-        method: Web3Event.UNLOCK_STATE_CHANGED,
-        params: {
-          isUnlocked,
-          ...(isUnlocked ? { accounts } : {}),
-        },
-      };
+    switchMap((lockedState) => {
+      return wallet$.pipe(
+        map((wallet) => {
+          const isUnlocked = lockedState?.locked;
+          const accounts =
+            isUnlocked && wallet ? getAccountsFromWallet(wallet) : [];
+          return {
+            method: Web3Event.UNLOCK_STATE_CHANGED,
+            params: {
+              isUnlocked,
+              ...(isUnlocked ? { accounts } : {}),
+            },
+          };
+        })
+      );
     })
   );
 }
