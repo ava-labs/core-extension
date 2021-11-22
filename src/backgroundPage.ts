f83a44768a1825c16055579cac39e919a4a14f2e
Oliver Wang <oliver.wang@avalabs.org>
2021-11-22 15:51:05 -0800

    changes to the background page revert and change the target to main branch

--- src/backgroundPage.ts
+++ src/backgroundPage.ts
@@ -9,7 +9,6 @@ import { ContextContainer } from './hooks/useIsSpecificContextContainer';
  * If they just install then they need to onboard and we force them
  * fullscreen
  */
-// test comment123asdfasdfasdf
 browser.runtime.onInstalled.addListener((details) => {
   if (details.reason === 'install') {
     browser.tabs.create({ url: ContextContainer.HOME });
