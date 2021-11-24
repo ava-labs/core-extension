a977ab64203bddfea90ae1d962038f04f5c9b5eb
Oliver Wang <oliver.wang@avalabs.org>
2021-11-24 14:21:12 -0800

    fix(take out comment)

--- src/background/services/accounts/events/accountsUpdatedEvent.ts
+++ src/background/services/accounts/events/accountsUpdatedEvent.ts
@@ -3,7 +3,6 @@ import { AccountsEvents } from './models';
 import { accounts$ } from '@src/background/services/accounts/accounts';
 
 export function accountsUpdateEvents() {
-  //TODO: take this comment out
   return accounts$.pipe(
     map((accounts) => {
       return {
