a7b26a8a3dd0bd88dbc1ff6fd2d9d430e8bbb978
Oliver Wang <oliver.wang@avalabs.org>
2021-11-26 12:16:20 -0800

    add comments and address comments

--- .releaserc.js
+++ .releaserc.js
@@ -1,3 +1,21 @@
+/*
+This file contains the configuration for semantic release, the library we use to tag the correct 
+semantic version numbers onto releases. We have two release paths, one on main and one on release branch. 
+
+In the code below we check the env variable RELEASE_BRANCH to decide what we should do. As of 
+the time of this file semantic release does not support specifying a config file from their CLI, 
+so this is the only we can have dynamic configs based on branch. 
+
+To test run this file, first get a github token at https://github.com/settings/tokens
+and add it to the GITHUB_TOKEN env variable then specify what branch you want to run (main or release) under RELEASE_BRANCH
+
+$ export GITHUB_TOKEN=<token>
+$ export RELEASE_BRANCH=<main or release>
+$ run yarn run semantic-release -d
+
+
+*/
+
 const commitAnalyzerSetting = [
   '@semantic-release/commit-analyzer',
   {
@@ -47,6 +65,14 @@ const execSetting = [
   },
 ];
 
+const execSettingPatchAnyCommits = [
+  '@semantic-release/exec',
+  {
+    prepareCmd: 'yarn zip',
+    analyzeCommitsCmd: 'echo patch',
+  },
+];
+
 const githubSetting = [
   '@semantic-release/github',
   {
@@ -73,12 +99,22 @@ const gitSetting = [
   },
 ];
 
-let plugins = [releaseReplaceSetting, execSetting, githubSetting, gitSetting];
-
+let plugins;
 if (process.env && process.env.RELEASE_BRANCH === 'release') {
-  plugins = [commitAnalyzerSetting, ...plugins];
+  plugins = [
+    commitAnalyzerSetting,
+    releaseReplaceSetting,
+    execSetting,
+    githubSetting,
+    gitSetting,
+  ];
 } else {
-  execSetting[1].analyzeCommitsCmd = 'echo patch';
+  plugins = [
+    releaseReplaceSetting,
+    execSettingPatchAnyCommits,
+    githubSetting,
+    gitSetting,
+  ];
 }
 
 module.exports = {
