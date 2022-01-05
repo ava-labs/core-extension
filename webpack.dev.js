f03a8bd171841ea7fe89bc3ebfaec59d303d491b
Connor Chevli <connorchev@gmail.com>
2022-01-05 08:54:44 -0500

    fix: use one instance of avalanche-wallet-sdk on dev (#134)

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 const { DefinePlugin } = require('webpack');
@@ -12,4 +13,18 @@ module.exports = merge(common, {
       },
     }),
   ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+    alias: {
+      '@avalabs/avalanche-wallet-sdk': path.resolve(
+        './node_modules/@avalabs/wallet-react-components/node_modules/@avalabs/avalanche-wallet-sdk'
+      ),
+    },
+    fallback: {
+      '@avalabs/avalanche-wallet-sdk': path.resolve(
+        `./node_modules/@avalabs/avalanche-wallet-sdk`
+      ),
+    },
+    symlinks: false,
+  },
 });
