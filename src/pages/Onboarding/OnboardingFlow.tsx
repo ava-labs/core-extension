b6067de78047cac573917328f1994c980d71a6fa
Cohan Carpentier <cohan.carpentier@gmail.com>
2022-01-10 08:56:48 -0500

    fix: using card props

--- src/pages/Onboarding/OnboardingFlow.tsx
+++ src/pages/Onboarding/OnboardingFlow.tsx
@@ -88,12 +88,10 @@ export function OnboardingFlow() {
       </HorizontalFlex>
       <VerticalFlex align="center" justify="center" grow="1">
         <Card
-          style={{
-            width: '600px',
-            minHeight: '540px',
-            height: 'calc(100vh - 130px)',
-            maxHeight: '800px',
-          }}
+          width="600px"
+          minHeight="540px"
+          height="calc(100vh - 130px)"
+          maxHeight="800px"
         >
           {content}
         </Card>
