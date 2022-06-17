895e2a8943d9d93fdb61cccc9470268e2d75f51e
Gergely Lovas <36536513+gergelylovas@users.noreply.github.com>
2022-06-17 19:33:40 +0200

    fix: remove debug line (#485)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -9,15 +9,11 @@ import { Portfolio } from './components/Portfolio/Portfolio';
 import { useSettingsContext } from '@src/contexts/SettingsProvider';
 import { useEffect, useState } from 'react';
 import { useOnboardingContext } from '@src/contexts/OnboardingProvider';
-import { useLedgerDisconnectedDialog } from '../SignTransaction/hooks/useLedgerDisconnectedDialog';
-import { LedgerAppType } from '@src/contexts/LedgerProvider';
 
 export function Home() {
   const { toggleIsDefaultExtension, isDefaultExtension } = useSettingsContext();
   const { updateInitialOpen, onboardingState } = useOnboardingContext();
   const [isSetAsDefaultDisplayed, setIsSetAsDefaultDisplayed] = useState(false);
-
-  useLedgerDisconnectedDialog(console.log, LedgerAppType.ETHEREUM);
 
   useEffect(() => {
     if (
