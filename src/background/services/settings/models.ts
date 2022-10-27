f3a49a692a1d5ec700de34b0cfee6eaf87b3e080
vvava <94397450+vvava@users.noreply.github.com>
2022-10-27 18:57:17 +0200

    feat: commented currencies (#702)

--- src/background/services/settings/models.ts
+++ src/background/services/settings/models.ts
@@ -29,18 +29,19 @@ export enum SettingsEvents {
   SETTINGS_UPDATED = 'SettingsEvents: SETTINGS_UPDATED',
 }
 
+// TODO: bring back the commented currenies when the glacier supports them
 export const CURRENCIES: {
   name: string;
   symbol: string;
 }[] = [
   { name: 'United States Dollar', symbol: 'USD' },
   { name: 'Euro', symbol: 'EUR' },
-  { name: 'Japanese Yen', symbol: 'JPY' },
+  // { name: 'Japanese Yen', symbol: 'JPY' },
   { name: 'Pound Sterling', symbol: 'GBP' },
   { name: 'Australian Dollar', symbol: 'AUD' },
   { name: 'Canadian Dollar', symbol: 'CAD' },
   { name: 'Swiss Franc', symbol: 'CHF' },
-  { name: 'Chinese Renminbi', symbol: 'CNH' },
+  // { name: 'Chinese Renminbi', symbol: 'CNH' },
   { name: 'Hong Kong Dollar', symbol: 'HKD' },
-  { name: 'New Zealand Dollar', symbol: 'NZD' },
+  // { name: 'New Zealand Dollar', symbol: 'NZD' },
 ];
