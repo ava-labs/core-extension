904dec9b1f017bf42746216e5cabaa38a6961fb6
Gergely Lovas <36536513+gergelylovas@users.noreply.github.com>
2022-11-08 17:34:17 +0100

    feat: (Sentry) CP-3631 track the prodessing time with Sentry  (#721)
    * feat: (Sentry) CP-3631 track the prodessing time with Sentry
    
    * feat: (Sentry) Adding another handler
    
    * feat: (Sentry) post code review
    
    * feat: (Sentry) More fixes
    
    Co-authored-by: Fumiko <fumikom@gmail.com>

--- src/background/services/balances/BalancesServiceBTC.ts
+++ src/background/services/balances/BalancesServiceBTC.ts
@@ -7,6 +7,7 @@ import { singleton } from 'tsyringe';
 import { Account } from '../accounts/models';
 import { SettingsService } from '../settings/SettingsService';
 import { TokenType, TokenWithBalance } from './models';
+import * as Sentry from '@sentry/browser';
 
 @singleton()
 export class BalancesServiceBTC {
@@ -25,6 +26,9 @@ export class BalancesServiceBTC {
     accounts: Account[],
     network: Network
   ): Promise<Record<string, Record<string, TokenWithBalance>>> {
+    const sentryTracker = Sentry.startTransaction({
+      name: 'BalancesServiceBTC: getBalances',
+    });
     const provider = await this.networkService.getBitcoinProvider();
     const selectedCurrency = (await this.settingsService.getSettings())
       .currency;
@@ -88,7 +92,7 @@ export class BalancesServiceBTC {
         [result.value.address]: result.value.balances,
       };
     }, {});
-
+    sentryTracker.finish();
     return balances;
   }
 }
