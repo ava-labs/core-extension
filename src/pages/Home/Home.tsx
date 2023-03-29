8a2e27cdcbf88782241f933d9d07ec62c4dd6f25
Fumiko Richards <6504659+frichards@users.noreply.github.com>
2023-03-29 14:57:37 -0600

    fix: (toaster) CP-5247 close the toaster when setting as default app (#967)

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -47,6 +47,7 @@ export function Home() {
               onClick={() => {
                 toggleIsDefaultExtension();
                 updateInitialOpen();
+                toast.remove(toaster);
               }}
             >
               {t('Yes')}
