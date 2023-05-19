f2380f082b7a51923cfc7586fa119a1b34eb5eb3
vvava <94397450+vvava@users.noreply.github.com>
2023-05-19 14:17:10 +0200

    feat: cp-5438 FAB migration to k2 (#1019)

--- src/components/common/fab/FAB.tsx
+++ src/components/common/fab/FAB.tsx
@@ -1,113 +1,115 @@
-import {
-  PlusIcon,
-  QRCodeIcon,
-  Typography,
-  SwitchIcon,
-  HorizontalFlex,
-  TextButton,
-  IconDirection,
-  BridgeIcon,
-  ArrowIcon,
-  Overlay,
-  VerticalFlex,
-  BuyIcon,
-} from '@avalabs/react-components';
-import styled, { useTheme } from 'styled-components';
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useIsFunctionAvailable } from '@src/hooks/useIsFunctionUnavailable';
 import { useTranslation } from 'react-i18next';
+import {
+  PlusIcon,
+  Stack,
+  Typography,
+  styled,
+  Backdrop,
+  QRCodeIcon,
+  SwapIcon,
+  BuyIcon,
+  BridgeIcon,
+  ArrowUpRightIcon,
+  useTheme,
+} from '@avalabs/k2-components';
 
-const ActionButtonWrapper = styled(TextButton)`
-  padding: 8px;
+const ActionButtonWrapper = styled(Stack)`
+  padding: 0 8px;
+  margin: 8px 0;
   border-radius: 0;
-  border-bottom: ${({ theme }) => `1px solid ${theme.colors.stroke2}66`};
-  width: 100%;
 
-  &:hover {
-    background: ${({ theme }) => `${theme.colors.bg1}0A`};
-  }
+  width: 100%;
 
   &:last-of-type {
     border-bottom: none;
   }
 `;
 
-const Menu = styled.div`
-  background: ${({ theme }) => theme.colors.text1};
-  border-radius: 13px;
-  width: 136px;
+const Menu = styled(Stack)<{ isOpen: boolean }>`
+  border-radius: 40px;
+  width: ${({ isOpen }) => (isOpen ? '80px' : '40px')};
   position: absolute;
   bottom: 100%;
-  right: 0;
+  right: ${({ isOpen }) => (isOpen ? 0 : '20px')};
   margin: 0 0 16px;
+  background: ${({ theme, isOpen }) =>
+    isOpen ? theme.palette.background.paper : 'transparent'};
+  flex-direction: column;
+  align-items: center;
+  transition: background-color 0.3s ease-out;
+  padding: 8px 0;
 `;
 
-const FabContainer = styled(VerticalFlex)<{ open: boolean }>`
+const MenuItems = styled(Stack)<{ isOpen: boolean }>`
+  transition: all 0.3s;
+  transform-origin: bottom;
+  transform: ${({ isOpen }) =>
+    isOpen ? 'translateY(0) scale(1)' : 'translateY(44px) scale(0)'};
+  opacity: ${({ isOpen }) => (isOpen ? '1' : '0')};
+`;
+
+const FabContainer = styled(Stack)`
   position: fixed;
   bottom: 16px;
   right: 16px;
   z-index: 9;
-
-  ${Menu} {
-    transition: all 0.2s;
-    transform-origin: bottom right;
-    transform: ${({ open }) =>
-      open ? 'translateY(0) scale(1)' : 'translateY(44px) scale(0)'};
-    opacity: ${({ open }) => (open ? '1' : '0')};
-  }
+  user-select: none;
 `;
 
-const FabButton = styled(TextButton)<{ open: boolean }>`
+const FabButton = styled(ActionButtonWrapper)<{ isOpen: boolean }>`
   width: 44px;
   height: 44px;
   display: flex;
   align-items: center;
   justify-content: center;
-  background: ${({ theme }) => theme.colors.text1};
   border-radius: 50%;
-
+  background-color: ${({ theme }) => theme.palette.common.white};
   transition: transform 0.2s ease-in-out;
-  transform: rotate(${({ open }) => (open ? '135deg' : '0')});
-`;
-
-const InvisibleOverlay = styled(Overlay)`
-  z-index: 8;
-  backdrop-filter: unset;
-  background-color: transparent;
+  transform: rotate(${({ isOpen }) => (isOpen ? '135deg' : '0')});
+  color: ${({ theme }) => theme.palette.background.paper};
+  margin: 8px;
+  cursor: pointer;
 `;
 
 export function FAB() {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-  const theme = useTheme();
   const history = useHistory();
   const { checkIsFunctionAvailable } = useIsFunctionAvailable();
   const { t } = useTranslation();
+  const theme = useTheme();
 
   const ActionButton = ({ icon, text, ...rest }) => (
     <ActionButtonWrapper
       data-testid={`${text.toLowerCase()}-action-button`}
       {...rest}
     >
-      <HorizontalFlex align="center" justify="flex-start" width="100%">
-        <HorizontalFlex
-          width="24px"
-          height="24px"
-          align="center"
-          justify="center"
+      <Stack
+        sx={{
+          alignItems: 'center',
+          width: '100%',
+          justifyContent: 'flex-start',
+        }}
+      >
+        <Stack
+          sx={{
+            alignItems: 'center',
+            width: '48px',
+            height: '48px',
+            justifyContent: 'center',
+            backgroundColor: 'common.white',
+            borderRadius: '50%',
+            color: 'text.secondary',
+            mb: 1,
+            cursor: 'pointer',
+          }}
         >
           {icon}
-        </HorizontalFlex>
-        <Typography
-          margin="0 0 0 8px"
-          size={14}
-          height="24px"
-          weight={600}
-          color={theme.colors.bg1}
-        >
-          {text}
-        </Typography>
-      </HorizontalFlex>
+        </Stack>
+        <Typography variant="caption">{text}</Typography>
+      </Stack>
     </ActionButtonWrapper>
   );
 
@@ -117,10 +119,9 @@ export function FAB() {
       route: '/send',
       name: 'Send',
       icon: (
-        <ArrowIcon
-          height="21px"
-          color={theme.colors.bg1}
-          direction={IconDirection.NORTHEAST}
+        <ArrowUpRightIcon
+          size={24}
+          sx={{ color: theme.palette.common.black }}
         />
       ),
     },
@@ -128,61 +129,58 @@ export function FAB() {
       text: t('Receive'),
       route: '/receive',
       name: 'Receive',
-      icon: <QRCodeIcon height="24px" color={theme.colors.bg1} />,
+      icon: <QRCodeIcon size={24} sx={{ color: theme.palette.common.black }} />,
     },
     {
       text: t('Buy'),
       route: '/buy',
       name: 'Buy',
-      icon: <BuyIcon height="21px" color={theme.colors.bg1} />,
+      icon: <BuyIcon size={24} sx={{ color: theme.palette.common.black }} />,
     },
     {
       text: t('Swap'),
       route: '/swap',
       name: 'Swap',
-      icon: (
-        <SwitchIcon
-          direction={IconDirection.RIGHT}
-          height="21px"
-          color={theme.colors.bg1}
-        />
-      ),
+      icon: <SwapIcon size={24} sx={{ color: theme.palette.common.black }} />,
     },
     {
       text: t('Bridge'),
       route: '/bridge',
       name: 'Bridge',
-      icon: <BridgeIcon height="24px" color={theme.colors.bg1} />,
+      icon: <BridgeIcon size={24} sx={{ color: theme.palette.common.black }} />,
     },
   ];
 
   return (
     <>
-      {isOpen && <InvisibleOverlay onClick={() => setIsOpen(false)} />}
-      <FabContainer open={isOpen}>
-        <FabButton
-          data-testid="action-menu-button"
-          open={isOpen}
-          onClick={() => {
-            setIsOpen(!isOpen);
-          }}
-        >
-          <PlusIcon height="16px" color={theme.colors.bg1} />
-        </FabButton>
-        <Menu>
-          {FABMenuItems.map(({ text, route, icon, name }) => {
-            if (!checkIsFunctionAvailable(name)) {
-              return null;
-            }
-            return (
-              <ActionButton
-                key={text}
-                text={text}
-                icon={icon}
-                onClick={() => history.push(route)}
-              />
-            );
-          })}
+      {isOpen && <Backdrop onClick={() => setIsOpen(false)} open={isOpen} />}
+      <FabContainer>
+        <Menu isOpen={isOpen}>
+          <MenuItems isOpen={isOpen}>
+            {isOpen &&
+              FABMenuItems.map(({ text, route, icon, name }) => {
+                if (!checkIsFunctionAvailable(name)) {
+                  return null;
+                }
+                return (
+                  <ActionButton
+                    key={text}
+                    text={text}
+                    icon={icon}
+                    onClick={() => history.push(route)}
+                  />
+                );
+              })}
+          </MenuItems>
+          <FabButton
+            data-testid="action-menu-button"
+            isOpen={isOpen}
+            onClick={() => {
+              setIsOpen(!isOpen);
+            }}
+          >
+            <PlusIcon size={24} />
+          </FabButton>
         </Menu>
       </FabContainer>
     </>
