dfcf636c7051ab136e1651d6cb898e889043f9fe
Michał Leszczyk <michal.leszczyk@avalabs.org>
2024-09-13 20:50:05 +0200

    chore: remove MATIC from balance cache (#46)

---
+++ src/background/services/storage/schemaMigrations/migrations/balances_v3.test.ts
@@ -0,0 +1,75 @@
+import balances_v3, { PreviousSchema } from './balances_v3';
+
+describe('background/services/storage/schemaMigrations/migrations/balances_v3', () => {
+  const previousValue: PreviousSchema = {
+    balances: {
+      '1': {
+        'address-1-a': {
+          'token-address-1-a': {
+            balance: 1,
+          },
+        },
+        'address-1-b': {
+          'token-address-1-b': {
+            balance: 2,
+          },
+        },
+      },
+      '137': {
+        'address-137-a': {
+          POL: {
+            balance: 4,
+          },
+          MATIC: {
+            balance: 4,
+          },
+          'token-address-137-a': {
+            balance: 3,
+          },
+        },
+        'address-137-b': {
+          POL: {
+            balance: 5,
+          },
+        },
+      },
+    },
+  };
+
+  it('removes MATIC from cached balances and leaves other tokens in tact', async () => {
+    const newBalances = await balances_v3.up(previousValue);
+
+    expect(newBalances).toEqual({
+      balances: {
+        '1': {
+          'address-1-a': {
+            'token-address-1-a': {
+              balance: 1,
+            },
+          },
+          'address-1-b': {
+            'token-address-1-b': {
+              balance: 2,
+            },
+          },
+        },
+        '137': {
+          'address-137-a': {
+            POL: {
+              balance: 4,
+            },
+            'token-address-137-a': {
+              balance: 3,
+            },
+          },
+          'address-137-b': {
+            POL: {
+              balance: 5,
+            },
+          },
+        },
+      },
+      version: 3,
+    });
+  });
+});
