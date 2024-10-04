45a7a7b86dcc343fc818e6b47588740c02814a59
Michał Leszczyk <michal.leszczyk@avalabs.org>
2024-10-04 11:46:08 +0200

    feat: provide custom headers to Glacier calls

--- src/background/services/wallet/handlers/avalanche_sendTransaction.ts
+++ src/background/services/wallet/handlers/avalanche_sendTransaction.ts
@@ -25,6 +25,7 @@ import { AnalyticsServicePosthog } from '../../analytics/AnalyticsServicePosthog
 import { ChainId } from '@avalabs/core-chains-sdk';
 import { openApprovalWindow } from '@src/background/runtime/openApprovalWindow';
 import { measureDuration } from '@src/utils/measureDuration';
+import { HEADERS } from '../../glacier/glacierConfig';
 
 type TxParams = {
   transactionHex: string;
@@ -102,6 +103,7 @@ export class AvalancheSendTransactionHandler extends DAppRequestHandler<
           isTestnet: !this.networkService.isMainnet(),
           url: process.env.GLACIER_URL as string,
           token: process.env.GLACIER_API_KEY,
+          headers: HEADERS,
         });
 
     if (vm === EVM) {
