393366c41c50d63f13d006e76f259f161cfb11f2
Viktor Vasas <viktor.vasas@avalabs.org>
2024-11-14 09:58:40 +0100

    feat: add duplicated account dialog

--- src/pages/ImportPrivateKey/ImportPrivateKey.tsx
+++ src/pages/ImportPrivateKey/ImportPrivateKey.tsx
@@ -22,12 +22,14 @@ import { useSettingsContext } from '@src/contexts/SettingsProvider';
 import { useBalanceTotalInCurrency } from '@src/hooks/useBalanceTotalInCurrency';
 import { networks } from 'bitcoinjs-lib';
 import { t } from 'i18next';
-import { KeyboardEvent, useEffect, useState } from 'react';
+import { KeyboardEvent, useCallback, useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { AccountsTab } from '../Accounts/Accounts';
 import { DerivedAddress, NetworkType } from './components/DerivedAddress';
 import { utils } from '@avalabs/avalanchejs';
 import { usePrivateKeyImport } from '../Accounts/hooks/usePrivateKeyImport';
+import { useAccountsContext } from '@src/contexts/AccountsProvider';
+import { DuplicatedAccountDialog } from './components/DuplicatedAccountDialog';
 
 type DerivedAddresses = {
   addressC: string;
@@ -49,14 +51,38 @@ export function ImportPrivateKey() {
   const balance = useBalanceTotalInCurrency(derivedAddresses as Account);
   const { isImporting: isImportLoading, importPrivateKey } =
     usePrivateKeyImport();
+  const { selectAccount } = useAccountsContext();
   const history = useHistory();
+  const { allAccounts } = useAccountsContext();
+  const [isKnownAccount, setIsKnownAccount] = useState(false);
+  const [isDuplicatedAccountDialogOpen, setIsDuplicatedDialogDialogOpen] =
+    useState(false);
+
+  const checkAccount = useCallback(
+    (address) => {
+      const findAccount = allAccounts.find(
+        ({ addressC }) => addressC.toLowerCase() === address.toLowerCase()
+      );
+      if (findAccount) {
+        setIsKnownAccount(true);
+      }
+    },
+    [allAccounts]
+  );
+
+  console.log('allAccounts: ', allAccounts);
 
   const isLoading = hasFocus && !derivedAddresses && !error;
 
   const handleImport = async () => {
     capture('ImportPrivateKeyClicked');
+    if (isKnownAccount && !isDuplicatedAccountDialogOpen) {
+      setIsDuplicatedDialogDialogOpen(true);
+      return;
+    }
     try {
-      await importPrivateKey(privateKey);
+      const importedAccountId = await importPrivateKey(privateKey);
+      await selectAccount(importedAccountId);
       toast.success(t('Private Key Imported'), { duration: 2000 });
       capture('ImportPrivateKeySucceeded');
       history.replace(`/accounts?activeTab=${AccountsTab.Imported}`);
@@ -77,7 +103,10 @@ export function ImportPrivateKey() {
     if (strippedPk.length === 64) {
       try {
         const publicKey = getPublicKeyFromPrivateKey(strippedPk);
+
         const addressC = getEvmAddressFromPubKey(publicKey);
+        checkAccount(addressC);
+
         const addressBTC = getBtcAddressFromPubKey(
           publicKey,
           network?.isTestnet ? networks.testnet : networks.bitcoin
@@ -94,7 +123,7 @@ export function ImportPrivateKey() {
     } else {
       errorHandler();
     }
-  }, [network?.isTestnet, privateKey]);
+  }, [checkAccount, isKnownAccount, network?.isTestnet, privateKey]);
 
   useEffect(() => {
     if (derivedAddresses && updateBalanceOnNetworks) {
@@ -106,106 +135,122 @@ export function ImportPrivateKey() {
   }, [derivedAddresses, updateBalanceOnNetworks]);
 
   return (
-    <Stack
-      sx={{
-        width: '100%',
-        height: '100%',
-        background: theme.palette.background.paper,
-      }}
-    >
-      <PageTitle margin={'24px 0 8px'} onBackClick={() => history.goBack()}>
-        {t('Import Private Key')}
-      </PageTitle>
-      <Stack sx={{ px: 2, gap: 3, height: '100%' }}>
-        <Stack sx={{ gap: 1.5 }}>
-          <TextField
-            autoFocus
-            onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
-              if (e.key === 'Enter') {
-                handleImport();
-              }
-            }}
-            data-testid="import-private-key-input"
-            fullWidth
-            label={t('Enter Private Key')}
-            inputLabelProps={{
-              sx: { transform: 'none', fontSize: 'body2.fontSize' },
-            }}
-            onChange={(e) => {
-              setPrivateKey(e.target.value);
-              if (!isFormDirty) setIsFormDirty(true);
-            }}
-            value={privateKey}
-            placeholder={t('Enter Private Key')}
-            type="password"
-            onFocus={() => setHasFocus(true)}
-            onBlur={() => setHasFocus(false)}
-          />
-          {error && isFormDirty ? (
-            <Typography variant="caption" color={theme.palette.error.main}>
-              {error}
-            </Typography>
-          ) : (
-            <Typography variant="caption">
-              {t('Add an account by entering a private key')}
-            </Typography>
-          )}
-        </Stack>
-
-        <Stack sx={{ gap: 1 }}>
-          <Typography variant="body2" sx={{ fontWeight: 'fontWeightSemibold' }}>
-            {t('Derived Addresses')}
-          </Typography>
-          <DerivedAddress
-            networkType={NetworkType.AVALANCHE}
-            address={derivedAddresses?.addressC ?? ''}
-            isLoading={isLoading}
-          />
-          <DerivedAddress
-            networkType={NetworkType.BITCOIN}
-            address={derivedAddresses?.addressBTC ?? ''}
-            isLoading={isLoading}
-          />
-        </Stack>
-
-        <Stack direction="row" sx={{ justifyContent: 'space-between' }}>
-          <Typography variant="body2" sx={{ fontWeight: 'fontWeightSemibold' }}>
-            {t('Total Balance')}
-          </Typography>
-          <Typography variant="body2">
-            {isBalanceLoading ? (
-              <CircularProgress size={16} />
-            ) : balance !== null && balance?.sum ? (
-              currencyFormatter(balance?.sum).replace(currency, '')
+    <>
+      {isKnownAccount && (
+        <DuplicatedAccountDialog
+          onClose={() => setIsDuplicatedDialogDialogOpen(false)}
+          onConfirm={() => handleImport()}
+          open={isDuplicatedAccountDialogOpen}
+        />
+      )}
+      <Stack
+        sx={{
+          width: '100%',
+          height: '100%',
+          background: theme.palette.background.paper,
+        }}
+      >
+        <PageTitle margin={'24px 0 8px'} onBackClick={() => history.goBack()}>
+          {t('Import Private Key')}
+        </PageTitle>
+        <Stack sx={{ px: 2, gap: 3, height: '100%' }}>
+          <Stack sx={{ gap: 1.5 }}>
+            <TextField
+              autoFocus
+              onKeyDown={(e: KeyboardEvent<HTMLInputElement>) => {
+                if (e.key === 'Enter') {
+                  handleImport();
+                }
+              }}
+              data-testid="import-private-key-input"
+              fullWidth
+              label={t('Enter Private Key')}
+              inputLabelProps={{
+                sx: { transform: 'none', fontSize: 'body2.fontSize' },
+              }}
+              onChange={(e) => {
+                setIsKnownAccount(false);
+                setPrivateKey(e.target.value);
+                if (!isFormDirty) setIsFormDirty(true);
+              }}
+              value={privateKey}
+              placeholder={t('Enter Private Key')}
+              type="password"
+              onFocus={() => setHasFocus(true)}
+              onBlur={() => setHasFocus(false)}
+            />
+            {error && isFormDirty ? (
+              <Typography variant="caption" color={theme.palette.error.main}>
+                {error}
+              </Typography>
             ) : (
-              '-'
+              <Typography variant="caption">
+                {t('Add an account by entering a private key')}
+              </Typography>
             )}
-          </Typography>
-        </Stack>
+          </Stack>
 
-        <Stack
-          sx={{
-            flexGrow: 1,
-            alignItems: 'center',
-            justifyContent: 'flex-end',
-            pt: 2,
-            pb: 3,
-          }}
-        >
-          <Button
-            data-testid="import-private-key-button"
-            size="large"
-            disabled={!derivedAddresses || isImportLoading}
-            fullWidth
-            onClick={handleImport}
-            isLoading={isImportLoading}
-            sx={{ gap: 1.5 }}
+          <Stack sx={{ gap: 1 }}>
+            <Typography
+              variant="body2"
+              sx={{ fontWeight: 'fontWeightSemibold' }}
+            >
+              {t('Derived Addresses')}
+            </Typography>
+            <DerivedAddress
+              networkType={NetworkType.AVALANCHE}
+              address={derivedAddresses?.addressC ?? ''}
+              isLoading={isLoading}
+            />
+            <DerivedAddress
+              networkType={NetworkType.BITCOIN}
+              address={derivedAddresses?.addressBTC ?? ''}
+              isLoading={isLoading}
+            />
+          </Stack>
+
+          <Stack direction="row" sx={{ justifyContent: 'space-between' }}>
+            <Typography
+              variant="body2"
+              sx={{ fontWeight: 'fontWeightSemibold' }}
+            >
+              {t('Total Balance')}
+            </Typography>
+            <Typography variant="body2">
+              {isBalanceLoading ? (
+                <CircularProgress size={16} />
+              ) : balance !== null && balance?.sum ? (
+                currencyFormatter(balance?.sum).replace(currency, '')
+              ) : (
+                '-'
+              )}
+            </Typography>
+          </Stack>
+
+          <Stack
+            sx={{
+              flexGrow: 1,
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+              pt: 2,
+              pb: 3,
+            }}
           >
-            <DownloadIcon size={16} />
-            {t('Import Private Key')}
-          </Button>
+            <Button
+              data-testid="import-private-key-button"
+              size="large"
+              disabled={!derivedAddresses || isImportLoading}
+              fullWidth
+              onClick={handleImport}
+              isLoading={isImportLoading}
+              sx={{ gap: 1.5 }}
+            >
+              <DownloadIcon size={16} />
+              {t('Import Private Key')}
+            </Button>
+          </Stack>
         </Stack>
       </Stack>
-    </Stack>
+    </>
   );
 }
