60acb5710a379ee17533abf1d15b19c58cf24181
bferenc <ferenc.benda@avalabs.org>
2024-12-12 00:23:46 +0100

    test: add solveChallenge tests

---
+++ src/background/services/appcheck/utils/solveChallenge.test.ts
@@ -0,0 +1,23 @@
+import { ChallengeTypes } from '../models';
+import solveBasicChallenge from './challenges/basic';
+import solveChallenge from './solveChallenge';
+
+jest.mock('./challenges/basic');
+
+describe('solveChallenge', () => {
+  const solution = 'solution';
+  const challengeDetails = '{}';
+
+  beforeEach(() => {
+    jest.resetAllMocks();
+
+    jest.mocked(solveBasicChallenge).mockReturnValueOnce(solution);
+  });
+
+  it('solves basic challenges correctly', async () => {
+    expect(
+      solveChallenge({ type: ChallengeTypes.BASIC, challengeDetails })
+    ).resolves.toBe(solution);
+    expect(solveBasicChallenge).toHaveBeenCalledWith(challengeDetails);
+  });
+});
