6192af52da1dde4d79f1494b35f71e87905ab5bf
Fumiko <fumikom@gmail.com>
2024-12-17 16:44:33 -0700

    fix: (Delete) handling multiple wallets and delete order error

--- src/background/services/accounts/handlers/avalanche_deleteAccounts.ts
+++ src/background/services/accounts/handlers/avalanche_deleteAccounts.ts
@@ -12,7 +12,21 @@ import { canSkipApproval } from '@src/utils/canSkipApproval';
 
 import { AccountsService } from '../AccountsService';
 import { Action } from '../../actions/models';
-import { Account, ImportedAccount, PrimaryAccount } from '../models';
+import { ImportedAccount, PrimaryAccount } from '../models';
+import { isPrimaryAccount } from '../utils/typeGuards';
+import { SecretsService } from '../../secrets/SecretsService';
+
+type PrimaryWalletAccounts = {
+  [walletId: string]: PrimaryAccount[];
+};
+
+export type DeleteAccountsDisplayData = {
+  primary: PrimaryWalletAccounts;
+  imported: ImportedAccount[];
+  wallet: {
+    [walletId: string]: string;
+  };
+};
 
 type Params = [accountIds: string[]];
 
@@ -23,7 +37,10 @@ export class AvalancheDeleteAccountsHandler extends DAppRequestHandler<
 > {
   methods = [DAppProviderRequest.ACCOUNTS_DELETE];
 
-  constructor(private accountsService: AccountsService) {
+  constructor(
+    private accountsService: AccountsService,
+    private secretsService: SecretsService
+  ) {
     super();
   }
 
@@ -32,6 +49,10 @@ export class AvalancheDeleteAccountsHandler extends DAppRequestHandler<
   ) => {
     const { request, scope } = rpcCall;
     const [accountIds] = request.params;
+
+    //TODO DELETE THIS
+    accountIds.unshift('1d6ad089-52b3-40b9-a3e5-ac5a970ba063');
+    accountIds.push('75b6b83d-2836-4997-a21f-3ee42f8e6a38');
 
     if (!request.site?.domain || !request.site?.tabId) {
       return {
@@ -42,17 +63,33 @@ export class AvalancheDeleteAccountsHandler extends DAppRequestHandler<
       };
     }
     console.log({ accountIds });
-    const accounts = accountIds.reduce((accumulator, accountId) => {
+
+    const currentAccounts = this.accountsService.getAccounts();
+    console.log({ currentAccounts });
+
+    const primaryWalletAccounts: PrimaryWalletAccounts = {};
+    const importedAccounts: ImportedAccount[] = [];
+
+    // Getting accounts by ids from params and organizing
+    for (const accountId of accountIds) {
       const account = this.accountsService.getAccountByID(accountId);
       if (account) {
-        accumulator[accountId] = account;
+        if (isPrimaryAccount(account)) {
+          if (primaryWalletAccounts[account.walletId]) {
+            primaryWalletAccounts[account.walletId]?.push(account);
+          } else {
+            primaryWalletAccounts[account.walletId] = [account];
+          }
+        } else {
+          importedAccounts.push(account);
+        }
       }
+    }
 
-      return accumulator;
-    }, {} as Record<string, ImportedAccount | PrimaryAccount>);
-    console.log({ accounts });
-
-    if (!Object.keys(accounts).length) {
+    if (
+      !Object.keys(primaryWalletAccounts).length &&
+      !importedAccounts.length
+    ) {
       return {
         ...request,
         error: ethErrors.rpc.invalidParams({
@@ -61,30 +98,79 @@ export class AvalancheDeleteAccountsHandler extends DAppRequestHandler<
       };
     }
 
-    if (await canSkipApproval(request.site.domain, request.site.tabId)) {
-      try {
-        await this.accountsService.deleteAccounts(Object.keys(accounts));
+    //Checking if the accounts to be deleted has the latest index
+    for (const [walletId, accountsInWallet] of Object.entries(
+      primaryWalletAccounts
+    )) {
+      console.log(`${walletId}: ${accountsInWallet}`);
+      accountsInWallet.sort((a, b) => b.index - a.index);
+      console.log({ sorted: accountsInWallet });
 
-        return {
-          ...request,
-          result: null,
-        };
-      } catch (err) {
-        console.log(err);
-        return {
-          ...request,
-          error: ethErrors.rpc.internal('Account removing failed'),
-        };
+      const walletAccounts =
+        this.accountsService.getPrimaryAccountsByWalletId(walletId);
+      console.log({ walletAccounts });
+
+      for (let i = 0; i < accountsInWallet.length; i++) {
+        const accountToDelete = accountsInWallet[i];
+        const accountInWallet = walletAccounts[walletAccounts.length - 1 - i];
+        if (
+          !accountToDelete ||
+          !accountInWallet ||
+          accountToDelete.id !== accountInWallet.id
+        ) {
+          return {
+            ...request,
+            error: ethErrors.rpc.invalidParams({
+              message: 'Only the last account of the wallet can be removed',
+            }),
+          };
+        }
+      }
+    }
+
+    //TODO Uncomment this block
+    // if (await canSkipApproval(request.site.domain, request.site.tabId)) {
+    //   try {
+    //     await this.accountsService.deleteAccounts(Object.keys(accounts));
+
+    //     return {
+    //       ...request,
+    //       result: null,
+    //     };
+    //   } catch (err) {
+    //     console.log(err);
+    //     return {
+    //       ...request,
+    //       error: ethErrors.rpc.internal('Account removing failed'),
+    //     };
+    //   }
+    // }
+
+    const walletNames: Record<string, string> = {};
+
+    const primaryWallets = await this.secretsService.getPrimaryWalletsDetails();
+
+    for (const walletId of Object.keys(primaryWalletAccounts)) {
+      const primaryWallet = primaryWallets.find(
+        (wallet) => wallet.id === walletId
+      );
+
+      if (primaryWallet?.name) {
+        walletNames[walletId] = primaryWallet.name;
       }
     }
 
     const actionData: Action<{
-      accounts: Record<string, ImportedAccount | PrimaryAccount>;
+      accounts: DeleteAccountsDisplayData;
     }> = {
       ...request,
       scope,
       displayData: {
-        accounts,
+        accounts: {
+          primary: primaryWalletAccounts,
+          imported: importedAccounts,
+          wallet: walletNames,
+        },
       },
     };
 
--- src/pages/Wallet/DeleteAccounts.tsx
+++ src/pages/Wallet/DeleteAccounts.tsx
@@ -5,11 +5,6 @@ import {
   Stack,
   Typography,
 } from '@avalabs/core-k2-components';
-
-import type {
-  ImportedAccount,
-  PrimaryAccount,
-} from '@src/background/services/accounts/models';
 import { useApproveAction } from '@src/hooks/useApproveAction';
 import { ActionStatus } from '@src/background/services/actions/models';
 import { useGetRequestId } from '@src/hooks/useGetRequestId';
@@ -21,13 +16,14 @@ import {
 import { WebsiteDetails } from '../SignTransaction/components/ApprovalTxDetails';
 import type { DomainMetadata } from '@src/background/models';
 import { truncateAddress } from '@src/utils/truncateAddress';
+import type { DeleteAccountsDisplayData } from '@src/background/services/accounts/handlers/avalanche_deleteAccounts';
 
 export function DeleteAccount() {
   const { t } = useTranslation();
   const requestId = useGetRequestId();
 
   const { action, updateAction, cancelHandler } = useApproveAction<{
-    accounts: Record<string, ImportedAccount | PrimaryAccount>;
+    accounts: DeleteAccountsDisplayData;
   }>(requestId);
 
   if (!action) {
@@ -45,6 +41,13 @@ export function DeleteAccount() {
     );
   }
 
+  const primaryAccountsCount = Object.values(
+    action.displayData.accounts.primary
+  ).flat().length;
+
+  const accountsCount =
+    action.displayData.accounts.imported.length + primaryAccountsCount;
+
   const site: DomainMetadata = action.site ?? {
     domain: '#',
     name: t('Unknown website'),
@@ -54,9 +57,7 @@ export function DeleteAccount() {
     <Stack sx={{ width: 1, px: 2 }}>
       <Stack sx={{ flexGrow: 1, width: 1, gap: 3 }}>
         <Typography variant="h4" sx={{ mt: 1.5, mb: 3.5 }}>
-          {Object.keys(action.displayData.accounts).length === 1
-            ? t('Delete account?')
-            : t('Delete accounts?')}
+          {accountsCount === 1 ? t('Delete Account?') : t('Delete Accounts?')}
         </Typography>
         <ApprovalSection>
           <ApprovalSectionHeader label={t('Action Details')} />
@@ -67,26 +68,75 @@ export function DeleteAccount() {
         <ApprovalSection>
           <ApprovalSectionHeader
             label={
-              Object.keys(action.displayData.accounts).length === 1
-                ? t('Account to delete')
-                : t('Accounts to delete')
+              accountsCount === 1
+                ? t('Account to be Deleted')
+                : t('Accounts to be Deleted')
             }
           />
           <ApprovalSectionBody sx={{ py: 1, px: 2, gap: 1 }}>
-            {Object.values(action.displayData.accounts).map((account, i) => (
-              <Stack
-                direction={'row'}
-                sx={{ justifyContent: 'space-between' }}
-                key={account.id}
-              >
-                <Typography>{account.name}</Typography>
-                <Typography>{truncateAddress(account.addressC)}</Typography>
+            {Object.keys(action.displayData.accounts.primary) &&
+              Object.entries(action.displayData.accounts.primary).map(
+                ([walletId, primaryAccounts], i) => (
+                  <Stack key={walletId} sx={{ mt: i === 0 ? 0 : 2 }}>
+                    <Stack>
+                      <Typography sx={{ fontWeight: 600, mb: 1 }}>
+                        {action.displayData.accounts.wallet[walletId]}
+                      </Typography>
+                    </Stack>
+                    {primaryAccounts.map((primaryAccount) => (
+                      <Stack
+                        direction={'row'}
+                        sx={{ justifyContent: 'space-between' }}
+                        key={primaryAccount.id}
+                      >
+                        <Typography>{primaryAccount.name}</Typography>
+                        <Typography>
+                          {truncateAddress(primaryAccount.addressC)}
+                        </Typography>
+                      </Stack>
+                    ))}
+                  </Stack>
+                )
+              )}
+
+            {action.displayData.accounts.imported.length && (
+              <Stack sx={{ mt: 2 }}>
+                <Typography sx={{ fontWeight: 600, mb: 1 }}>
+                  {action.displayData.accounts.imported.length === 1
+                    ? t('Imported Account')
+                    : t('Imported Accounts')}
+                </Typography>
+                {action.displayData.accounts.imported.map((importedAccount) => (
+                  <Stack key={importedAccount.id}>
+                    <Stack
+                      direction={'row'}
+                      sx={{ justifyContent: 'space-between' }}
+                      key={importedAccount.id}
+                    >
+                      <Typography>{importedAccount.name}</Typography>
+                      <Typography>
+                        {truncateAddress(importedAccount.addressC)}
+                      </Typography>
+                    </Stack>
+                  </Stack>
+                ))}
               </Stack>
-            ))}
+            )}
           </ApprovalSectionBody>
         </ApprovalSection>
       </Stack>
       <Stack sx={{ width: 1, justifyContent: 'space-between' }}>
+        <Stack>
+          <Typography
+            variant="caption"
+            color="text.secondary"
+            sx={{ textAlign: 'center', mb: 2 }}
+          >
+            {t(
+              'Removing the account will delete all local account information stored on this computer. Your assets on chain will remain on chain.'
+            )}
+          </Typography>
+        </Stack>
         <Stack direction="row" sx={{ gap: 1 }}>
           <Button
             color="secondary"
@@ -104,6 +154,7 @@ export function DeleteAccount() {
             data-testid="switch-account-approve-btn"
             size="large"
             color="primary"
+            sx={{ color: 'error.main' }}
             onClick={() => {
               updateAction({
                 status: ActionStatus.SUBMITTING,
@@ -112,7 +163,7 @@ export function DeleteAccount() {
             }}
             fullWidth
           >
-            {t('Approve')}
+            {t('Delete')}
           </Button>
         </Stack>
       </Stack>
