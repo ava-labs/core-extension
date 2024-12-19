823b25c04974a0bb2df39916aa257c286be23d6f
Michał Leszczyk <michal.leszczyk@avalabs.org>
2024-12-19 12:33:29 +0100

    fix: linting

--- src/background/utils/getAddressResolutionOptions.ts
+++ src/background/utils/getAddressResolutionOptions.ts
@@ -2,7 +2,7 @@ import { NetworkService } from '../services/network/NetworkService';
 import { AddressResolutionOptions } from '../models';
 
 export const getAddressResolutionOptions = async (
-  networkService: NetworkService
+  networkService: NetworkService,
 ): Promise<AddressResolutionOptions> => ({
   isMainnet: networkService.isMainnet(),
   providerXP: await networkService.getAvalanceProviderXP(),
