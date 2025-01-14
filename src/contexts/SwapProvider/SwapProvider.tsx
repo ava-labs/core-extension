7ff2f2d9dc916c6fc10eea6718eebb606207b715
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-01-14 12:14:38 +0100

    refactor: extract some common utils for SwapProvider

--- src/contexts/SwapProvider/SwapProvider.tsx
+++ src/contexts/SwapProvider/SwapProvider.tsx
@@ -1,27 +1,18 @@
 import { createContext, useCallback, useContext, useMemo } from 'react';
+import type { JsonRpcBatchInternal } from '@avalabs/core-wallets-sdk';
 import { useConnectionContext } from '../ConnectionProvider';
-import { OptimalRate, SwapSide } from 'paraswap-core';
+import { SwapSide } from 'paraswap-core';
 import browser from 'webextension-polyfill';
-import {
-  APIError,
-  Address,
-  BuildOptions,
-  ETHER_ADDRESS,
-  ParaSwap,
-  PriceString,
-  Transaction,
-} from 'paraswap';
+import { APIError, ETHER_ADDRESS, ParaSwap, Transaction } from 'paraswap';
 import { useNetworkContext } from '../NetworkProvider';
 import { useAccountsContext } from '../AccountsProvider';
 import { useFeatureFlagContext } from '../FeatureFlagsProvider';
 import { FeatureGates } from '@src/background/services/featureFlags/models';
 import { ChainId } from '@avalabs/core-chains-sdk';
-import ERC20 from '@openzeppelin/contracts/build/contracts/ERC20.json';
 import Web3 from 'web3';
 import { incrementalPromiseResolve } from '@src/utils/incrementalPromiseResolve';
 import Big from 'big.js';
 import { resolve } from '@src/utils/promiseResolver';
-import { ethers } from 'ethers';
 import { RpcMethod, TokenType } from '@avalabs/vm-module-types';
 import { useTokensWithBalances } from '@src/hooks/useTokensWithBalances';
 import { BN } from 'bn.js';
@@ -30,15 +21,23 @@ import { useNetworkFeeContext } from '../NetworkFeeProvider';
 import { useTranslation } from 'react-i18next';
 import {
   GetRateParams,
-  PARASWAP_RETRYABLE_ERRORS,
   ParaswapPricesResponse,
   SwapContextAPI,
   SwapParams,
-  hasParaswapError,
   DISALLOWED_SWAP_ASSETS,
+  BuildTxParams,
 } from './models';
 import Joi from 'joi';
 import { isAPIError } from '@src/pages/Swap/utils';
+import {
+  checkForErrorsInBuildTxResult,
+  checkForErrorsInGetRateResult,
+  ensureAllowance,
+  throwError,
+  validateParams,
+} from './swap-utils';
+import { assertPresent } from '@src/utils/assertions';
+import { CommonError } from '@src/utils/errors';
 
 export const SwapContext = createContext<SwapContextAPI>({} as any);
 
@@ -118,40 +117,9 @@ export function SwapContextProvider({ children }: { children: any }) {
         return response.json();
       };
 
-      function checkForErrorsInResult(
-        response: ParaswapPricesResponse | TypeError,
-      ) {
-        const isFetchError = response instanceof TypeError;
-        const isParaswapError = !isFetchError && hasParaswapError(response);
-
-        if (isFetchError || isParaswapError) {
-          // If there is an error, we may want to retry the request if a network issue
-          // or some of the documented Paraswap API errors occurred.
-          const isNetworkIssue =
-            isFetchError && response.message === 'Failed to fetch';
-          const shouldBeRetried =
-            isNetworkIssue ||
-            (isParaswapError &&
-              PARASWAP_RETRYABLE_ERRORS.includes(response.error));
-
-          if (shouldBeRetried) {
-            return true;
-            // If an error occurred, but there is no point in retrying a request,
-            // we need to propagate the error so we're able to show an approriate
-            // message in the UI.
-          } else if (isFetchError) {
-            throw response;
-          } else {
-            throw new Error(response.error);
-          }
-        }
-
-        return false;
-      }
-
       const result = await incrementalPromiseResolve<ParaswapPricesResponse>(
         () => optimalRates(),
-        checkForErrorsInResult,
+        checkForErrorsInGetRateResult,
       );
 
       return {
@@ -178,24 +146,18 @@ export function SwapContextProvider({ children }: { children: any }) {
   }, [paraswap, featureFlags]);
 
   const buildTx = useCallback(
-    async (
-      network: string,
-      srcToken: Address,
-      destToken: Address,
-      srcAmount: PriceString,
-      destAmount: PriceString,
-      priceRoute: OptimalRate,
-      userAddress: Address,
-      partner?: string,
-      partnerAddress?: string,
-      partnerFeeBps?: number,
-      receiver?: Address,
-      options?: BuildOptions,
-      srcDecimals?: number,
-      destDecimals?: number,
-      permit?: string,
-      deadline?: string,
-    ) => {
+    async ({
+      network,
+      srcToken,
+      destToken,
+      srcAmount,
+      destAmount,
+      srcDecimals,
+      destDecimals,
+      priceRoute,
+      userAddress,
+      ignoreChecks,
+    }: BuildTxParams) => {
       if (!featureFlags[FeatureGates.SWAP]) {
         throw new Error(`Feature (SWAP) is currently unavailable`);
       }
@@ -210,10 +172,7 @@ export function SwapContextProvider({ children }: { children: any }) {
         gasPrice: Joi.string().optional(),
       }).unknown();
 
-      const query = new URLSearchParams(options as Record<string, string>);
-      const txURL = `${
-        (paraswap as any).apiURL
-      }/transactions/${network}/?${query.toString()}`;
+      const txURL = `${(paraswap as any).apiURL}/transactions/${network}`;
       const txConfig = {
         srcToken,
         srcDecimals,
@@ -223,12 +182,8 @@ export function SwapContextProvider({ children }: { children: any }) {
         destAmount,
         priceRoute,
         userAddress,
-        partner,
-        partnerAddress,
-        partnerFeeBps,
-        receiver,
-        permit,
-        deadline,
+        partner: 'Avalanche',
+        ignoreChecks,
       };
 
       const response = await fetch(txURL, {
@@ -256,228 +211,34 @@ export function SwapContextProvider({ children }: { children: any }) {
     [featureFlags, paraswap],
   );
 
-  const swap = useCallback(
+  const notifyOnSwapResult = useCallback(
     async ({
+      provider,
+      txHash,
+      userAddress,
       srcToken,
       destToken,
+      srcAmount,
+      destAmount,
       srcDecimals,
       destDecimals,
-      srcAmount,
-      priceRoute,
-      destAmount,
-      gasLimit,
-      slippage,
-    }: SwapParams) => {
-      if (!srcToken) {
-        throw new Error('Missing parameter: srcToken');
-      }
-
-      if (!destToken) {
-        throw new Error('Missing parameter: destToken');
-      }
-
-      if (!srcAmount) {
-        throw new Error('Missing parameter: srcAmount');
-      }
-
-      if (!srcDecimals) {
-        throw new Error('Missing parameter: srcDecimals');
-      }
-
-      if (!destDecimals) {
-        throw new Error('Missing parameter: destDecimals');
-      }
-
-      if (!destAmount) {
-        throw new Error('Missing parameter: destAmount');
-      }
-
-      if (!priceRoute) {
-        throw new Error('Missing parameter: priceRoute');
-      }
-
-      if (!gasLimit) {
-        throw new Error('Missing parameter: gasLimit');
-      }
-
-      if (!activeNetwork || activeNetwork.isTestnet) {
-        throw new Error(`Unsupported network: ${activeNetwork?.chainId}`);
-      }
-
-      if (!networkFee) {
-        throw new Error('Unknown network fee');
-      }
-
-      const srcTokenAddress =
-        srcToken === activeNetwork.networkToken.symbol
-          ? ETHER_ADDRESS
-          : srcToken;
-      const destTokenAddress =
-        destToken === activeNetwork.networkToken.symbol
-          ? ETHER_ADDRESS
-          : destToken;
-
-      if (!activeAccount?.addressC) {
-        throw new Error('Account address missing');
-      }
-
-      if (!avaxProviderC) {
-        throw new Error('RPC provider is not available');
-      }
-
-      const buildOptions = undefined,
-        partnerAddress = undefined,
-        partner = 'Avalanche',
-        userAddress = activeAccount.addressC,
-        receiver = undefined,
-        permit = undefined,
-        deadline = undefined,
-        partnerFeeBps = undefined;
-
-      const spender = await getParaswapSpender();
-
-      let approveTxHash;
-
-      const minAmount = new Big(priceRoute.destAmount)
-        .times(1 - slippage / 100)
-        .toFixed(0);
-
-      const maxAmount = new Big(srcAmount).times(1 + slippage / 100).toFixed(0);
-
-      //TODO: it may fail when we want to swap erc20 tokens -> investigate
-      const sourceAmount = priceRoute.side === 'SELL' ? srcAmount : maxAmount;
-
-      const destinationAmount =
-        priceRoute.side === 'SELL' ? minAmount : priceRoute.destAmount;
-
-      // no need to approve AVAX
-      if (srcToken !== activeNetwork.networkToken.symbol) {
-        const contract = new ethers.Contract(
-          srcTokenAddress,
-          ERC20.abi,
-          avaxProviderC,
-        );
-
-        if (!contract.allowance) {
-          throw new Error(`Allowance Conract Error`);
-        }
-
-        const [allowance, allowanceError] = await resolve(
-          contract.allowance(userAddress, spender),
-        );
-
-        if (allowanceError) {
-          throw new Error(`Allowance Error: ${allowanceError}`);
-        }
-
-        if (allowance < sourceAmount) {
-          const [approveGasLimit] = await resolve(
-            contract.approve!.estimateGas(spender, sourceAmount),
-          );
-
-          if (allowance < sourceAmount) {
-            const { data } = await contract.approve!.populateTransaction(
-              spender,
-              sourceAmount,
-            );
-            const [hash, signError] = await resolve(
-              request({
-                method: RpcMethod.ETH_SEND_TRANSACTION,
-                params: [
-                  {
-                    chainId: ChainId.AVALANCHE_MAINNET_ID.toString(),
-                    gas:
-                      '0x' + Number(approveGasLimit || gasLimit).toString(16),
-                    data,
-                    from: activeAccount.addressC,
-                    to: srcTokenAddress,
-                  },
-                ],
-              }),
-            );
-
-            if (signError) {
-              throwError(signError);
-            }
-            approveTxHash = hash;
-          } else {
-            approveTxHash = [];
-          }
-        }
-      }
-
-      function checkForErrorsInResult(result: Transaction | APIError) {
-        return (
-          (result as APIError).message === 'Server too busy' ||
-          // paraswap returns responses like this: {error: 'Not enough 0x4f60a160d8c2dddaafe16fcc57566db84d674…}
-          // when they are too slow to detect the approval
-          (result as any).error ||
-          result instanceof Error
-        );
-      }
-
-      const [txBuildData, txBuildDataError] = await resolve(
-        incrementalPromiseResolve(
-          () =>
-            buildTx(
-              ChainId.AVALANCHE_MAINNET_ID.toString(),
-              srcTokenAddress,
-              destTokenAddress,
-              sourceAmount,
-              destinationAmount,
-              priceRoute,
-              userAddress,
-              partner,
-              partnerAddress,
-              partnerFeeBps,
-              receiver,
-              buildOptions,
-              activeNetwork.networkToken.symbol === srcToken ? 18 : srcDecimals,
-              activeNetwork.networkToken.symbol === destToken
-                ? 18
-                : destDecimals,
-              permit,
-              deadline,
-            ),
-          checkForErrorsInResult,
-          0,
-          10,
-        ),
-      );
-
-      if (txBuildDataError) {
-        throw new Error(`Data Error: ${txBuildDataError}`);
-      }
-
-      const [swapTxHash, signError] = await resolve(
-        request({
-          method: RpcMethod.ETH_SEND_TRANSACTION,
-          params: [
-            {
-              chainId: ChainId.AVALANCHE_MAINNET_ID.toString(),
-              gas: '0x' + Number(txBuildData.gas).toString(16),
-              data: txBuildData.data,
-              to: txBuildData.to,
-              from: activeAccount.addressC,
-              value:
-                srcToken === activeNetwork.networkToken.symbol
-                  ? `0x${new BN(sourceAmount).toString('hex')}`
-                  : undefined, // AVAX value needs to be sent with the transaction
-            },
-          ],
-        }),
-      );
-
-      if (signError) {
-        throwError(signError);
-      }
-
-      avaxProviderC.waitForTransaction(swapTxHash).then(async (tx) => {
+    }: {
+      provider: JsonRpcBatchInternal;
+      txHash: string;
+      userAddress: string;
+      srcToken: string;
+      destToken: string;
+      srcAmount: string;
+      destAmount: string;
+      srcDecimals: number;
+      destDecimals: number;
+    }) => {
+      provider.waitForTransaction(txHash).then(async (tx) => {
         const isSuccessful = tx && tx.status === 1;
 
         captureEncrypted(isSuccessful ? 'SwapSuccessful' : 'SwapFailed', {
           address: userAddress,
-          txHash: swapTxHash,
+          txHash: txHash,
           chainId: ChainId.AVALANCHE_MAINNET_ID,
         });
 
@@ -518,23 +279,137 @@ export function SwapContextProvider({ children }: { children: any }) {
               ),
         });
       });
+    },
+    [captureEncrypted, findSymbol, t],
+  );
 
-      return {
-        swapTxHash,
-        approveTxHash,
-      };
+  const swap = useCallback(
+    async (params: SwapParams) => {
+      assertPresent(activeNetwork, CommonError.NoActiveNetwork);
+      assertPresent(networkFee, CommonError.UnknownNetworkFee);
+      assertPresent(activeAccount, CommonError.NoActiveAccount);
+      assertPresent(avaxProviderC, CommonError.Unknown);
+
+      const {
+        srcToken,
+        destToken,
+        srcAmount,
+        srcDecimals,
+        destDecimals,
+        destAmount,
+        priceRoute,
+        slippage,
+      } = validateParams(params);
+
+      const srcTokenAddress =
+        srcToken === activeNetwork.networkToken.symbol
+          ? ETHER_ADDRESS
+          : srcToken;
+      const destTokenAddress =
+        destToken === activeNetwork.networkToken.symbol
+          ? ETHER_ADDRESS
+          : destToken;
+
+      const userAddress = activeAccount.addressC;
+
+      const spender = await getParaswapSpender();
+
+      const minAmount = new Big(priceRoute.destAmount)
+        .times(1 - slippage / 100)
+        .toFixed(0);
+
+      const maxAmount = new Big(srcAmount).times(1 + slippage / 100).toFixed(0);
+      const sourceAmount = priceRoute.side === 'SELL' ? srcAmount : maxAmount;
+
+      const destinationAmount =
+        priceRoute.side === 'SELL' ? minAmount : priceRoute.destAmount;
+
+      // no need to approve AVAX
+      if (srcToken !== activeNetwork.networkToken.symbol) {
+        await ensureAllowance({
+          amount: BigInt(sourceAmount),
+          provider: avaxProviderC,
+          request,
+          spenderAddress: spender,
+          tokenAddress: srcTokenAddress,
+          userAddress,
+        });
+      }
+
+      const [txBuildData, txBuildDataError] = await resolve(
+        incrementalPromiseResolve(
+          () =>
+            buildTx({
+              network: ChainId.AVALANCHE_MAINNET_ID.toString(),
+              srcToken: srcTokenAddress,
+              destToken: destTokenAddress,
+              srcAmount: sourceAmount,
+              destAmount: destinationAmount,
+              priceRoute,
+              userAddress,
+              srcDecimals:
+                activeNetwork.networkToken.symbol === srcToken
+                  ? 18
+                  : srcDecimals,
+              destDecimals:
+                activeNetwork.networkToken.symbol === destToken
+                  ? 18
+                  : destDecimals,
+            }),
+          checkForErrorsInBuildTxResult,
+          0,
+          10,
+        ),
+      );
+
+      if (txBuildDataError) {
+        throw new Error(`Data Error: ${txBuildDataError}`);
+      }
+
+      const [swapTxHash, signError] = await resolve(
+        request({
+          method: RpcMethod.ETH_SEND_TRANSACTION,
+          params: [
+            {
+              chainId: ChainId.AVALANCHE_MAINNET_ID.toString(),
+              gas: '0x' + Number(txBuildData.gas).toString(16),
+              data: txBuildData.data,
+              to: txBuildData.to,
+              from: activeAccount.addressC,
+              value:
+                srcToken === activeNetwork.networkToken.symbol
+                  ? `0x${new BN(sourceAmount).toString('hex')}`
+                  : undefined, // AVAX value needs to be sent with the transaction
+            },
+          ],
+        }),
+      );
+
+      if (signError) {
+        throwError(signError);
+      }
+
+      notifyOnSwapResult({
+        provider: avaxProviderC,
+        txHash: swapTxHash,
+        userAddress,
+        srcToken,
+        destToken,
+        srcAmount,
+        destAmount,
+        srcDecimals,
+        destDecimals,
+      });
     },
     [
-      activeAccount?.addressC,
+      activeAccount,
       activeNetwork,
       avaxProviderC,
       buildTx,
-      captureEncrypted,
       getParaswapSpender,
       networkFee,
       request,
-      t,
-      findSymbol,
+      notifyOnSwapResult,
     ],
   );
 
@@ -549,14 +424,6 @@ export function SwapContextProvider({ children }: { children: any }) {
     </SwapContext.Provider>
   );
 }
-
-const throwError = (err: string | unknown): never => {
-  if (typeof err === 'string') {
-    throw new Error(err);
-  }
-
-  throw err;
-};
 
 export function useSwapContext() {
   return useContext(SwapContext);
---
+++ src/contexts/SwapProvider/swap-utils.ts
@@ -0,0 +1,216 @@
+import { APIError, Transaction } from 'paraswap';
+import { Contract, TransactionRequest } from 'ethers';
+import { ChainId } from '@avalabs/core-chains-sdk';
+import { RpcMethod } from '@avalabs/vm-module-types';
+import { JsonRpcBatchInternal } from '@avalabs/core-wallets-sdk';
+import ERC20 from '@openzeppelin/contracts/build/contracts/ERC20.json';
+
+import { resolve } from '@src/utils/promiseResolver';
+import { RequestHandlerType } from '@src/background/connections/models';
+
+import {
+  PARASWAP_RETRYABLE_ERRORS,
+  ParaswapPricesResponse,
+  SwapParams,
+  hasParaswapError,
+} from './models';
+
+export function validateParams(
+  params: Partial<SwapParams>,
+): Required<SwapParams> | never {
+  const {
+    srcToken,
+    destToken,
+    srcAmount,
+    srcDecimals,
+    destDecimals,
+    destAmount,
+    priceRoute,
+    slippage,
+  } = params;
+
+  if (!srcToken) {
+    throw new Error('Missing parameter: srcToken');
+  }
+
+  if (!destToken) {
+    throw new Error('Missing parameter: destToken');
+  }
+
+  if (!srcAmount) {
+    throw new Error('Missing parameter: srcAmount');
+  }
+
+  if (!srcDecimals) {
+    throw new Error('Missing parameter: srcDecimals');
+  }
+
+  if (!destDecimals) {
+    throw new Error('Missing parameter: destDecimals');
+  }
+
+  if (!destAmount) {
+    throw new Error('Missing parameter: destAmount');
+  }
+
+  if (!priceRoute) {
+    throw new Error('Missing parameter: priceRoute');
+  }
+
+  if (!slippage) {
+    throw new Error('Missing parameter: slippage');
+  }
+
+  return {
+    srcToken,
+    destToken,
+    srcAmount,
+    srcDecimals,
+    destDecimals,
+    destAmount,
+    priceRoute,
+    slippage,
+  };
+}
+
+export async function buildApprovalTx({
+  contract,
+  userAddress,
+  spenderAddress,
+  tokenAddress,
+  amount,
+  provider,
+}: {
+  contract: Contract;
+  userAddress: string;
+  spenderAddress: string;
+  tokenAddress: string;
+  amount: bigint;
+  provider: JsonRpcBatchInternal;
+}) {
+  const { data } = await contract.approve!.populateTransaction(
+    spenderAddress,
+    amount,
+  );
+
+  const tx: TransactionRequest = {
+    from: userAddress,
+    to: tokenAddress,
+    chainId: `0x${ChainId.AVALANCHE_MAINNET_ID.toString(16)}`,
+    data,
+  };
+  const [approvalGasLimit, approvalGasLimitError] = await resolve(
+    provider.estimateGas(tx),
+  );
+
+  if (approvalGasLimitError) {
+    throw new Error(
+      `Unable to estimate gas limit for allowance approval. Error: ${approvalGasLimitError}`,
+    );
+  }
+
+  return {
+    ...tx,
+    gas: `0x${approvalGasLimit.toString(16)}`,
+  };
+}
+
+export async function ensureAllowance({
+  provider,
+  tokenAddress,
+  userAddress,
+  spenderAddress,
+  amount,
+  request,
+}: {
+  provider: JsonRpcBatchInternal;
+  userAddress: string;
+  spenderAddress: string;
+  tokenAddress: string;
+  amount: bigint;
+  request: RequestHandlerType;
+}) {
+  const contract = new Contract(tokenAddress, ERC20.abi, provider);
+
+  if (!contract.allowance) {
+    throw new Error(`Allowance Conract Error`);
+  }
+
+  const [allowance, allowanceError] = await resolve(
+    contract.allowance(userAddress, spenderAddress),
+  );
+
+  if (allowanceError) {
+    throw new Error(`Allowance Fetching Error: ${allowanceError}`);
+  }
+
+  if (allowance < amount) {
+    const tx = await buildApprovalTx({
+      amount,
+      contract,
+      provider,
+      spenderAddress,
+      tokenAddress,
+      userAddress,
+    });
+
+    const [, signError] = await resolve(
+      request({
+        method: RpcMethod.ETH_SEND_TRANSACTION,
+        params: [tx],
+      }),
+    );
+
+    if (signError) {
+      throwError(signError);
+    }
+  }
+}
+
+export const throwError = (err: string | unknown): never => {
+  if (typeof err === 'string') {
+    throw new Error(err);
+  }
+
+  throw err;
+};
+
+export function checkForErrorsInGetRateResult(
+  response: ParaswapPricesResponse | TypeError,
+) {
+  const isFetchError = response instanceof TypeError;
+  const isParaswapError = !isFetchError && hasParaswapError(response);
+
+  if (isFetchError || isParaswapError) {
+    // If there is an error, we may want to retry the request if a network issue
+    // or some of the documented Paraswap API errors occurred.
+    const isNetworkIssue =
+      isFetchError && response.message === 'Failed to fetch';
+    const shouldBeRetried =
+      isNetworkIssue ||
+      (isParaswapError && PARASWAP_RETRYABLE_ERRORS.includes(response.error));
+
+    if (shouldBeRetried) {
+      return true;
+      // If an error occurred, but there is no point in retrying a request,
+      // we need to propagate the error so we're able to show an approriate
+      // message in the UI.
+    } else if (isFetchError) {
+      throw response;
+    } else {
+      throw new Error(response.error);
+    }
+  }
+
+  return false;
+}
+
+export function checkForErrorsInBuildTxResult(result: Transaction | APIError) {
+  return (
+    (result as APIError).message === 'Server too busy' ||
+    // paraswap returns responses like this: {error: 'Not enough 0x4f60a160d8c2dddaafe16fcc57566db84d674…}
+    // when they are too slow to detect the approval
+    (result as any).error ||
+    result instanceof Error
+  );
+}
