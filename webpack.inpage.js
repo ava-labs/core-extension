df0f7513a6383204ea7fe6d6ca3ff50c296286ec
Viktor Vasas <viktor.vasas@avalabs.org>
2025-02-19 21:50:39 +0100

    fix: wallet selector icon, blue icons

--- webpack.inpage.js
+++ webpack.inpage.js
@@ -4,14 +4,17 @@ const { DefinePlugin } = require('webpack');
 
 const fs = require('fs');
 
-const svgPath = path.resolve(__dirname, 'src/images/favicon.svg');
+const svgPath = path.resolve(
+  __dirname,
+  'src/images/Core_logo_white_text_black_background.svg',
+);
 const svgCoreIcon = fs.readFileSync(svgPath, 'base64');
 
 const devEvmProviderConfig = {
   EVM_PROVIDER_INFO_NAME: '"Core Dev"',
   EVM_PROVIDER_INFO_UUID: '"b11db586-a24d-4d95-81c1-62bb8e9da7b5"',
   EVM_PROVIDER_INFO_ICON: JSON.stringify(
-    `data:image/svg+xml;base64,${svgCoreIcon}`
+    `data:image/svg+xml;base64,${svgCoreIcon}`,
   ),
   EVM_PROVIDER_INFO_DESCRIPTION: '"Core Dev Browser Extension"',
   EVM_PROVIDER_INFO_RDNS: '"app.core.extension"',
@@ -21,7 +24,7 @@ const prodEvmProviderConfig = {
   EVM_PROVIDER_INFO_NAME: '"Core"',
   EVM_PROVIDER_INFO_UUID: '"eafb8ae4-8535-48df-b8c1-e69b999c367d"',
   EVM_PROVIDER_INFO_ICON: JSON.stringify(
-    `data:image/svg+xml;base64,${svgCoreIcon}`
+    `data:image/svg+xml;base64,${svgCoreIcon}`,
   ),
   EVM_PROVIDER_INFO_DESCRIPTION: '"Core | Crypto Wallet & NFT Extension"',
   EVM_PROVIDER_INFO_RDNS: '"app.core.extension"',
