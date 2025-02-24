8a7742670a7f2b3e2f12a63ae0e359228ce36618
Viktor Vasas <viktor.vasas@avalabs.org>
2025-02-24 18:14:32 +0100

    feat: add gasless sdk

--- src/offscreen.ts
+++ src/offscreen.ts
@@ -1,14 +1,25 @@
 import browser, { Runtime } from 'webextension-polyfill';
 import { OFFSCREEN_SCRIPT } from './common';
+import { GaslessSdk } from '@avalabs/core-gasless-sdk';
 
+// console.log('fetchChallenge: ', fetchChallenge);
 console.log('offsreen starting....');
 const connection: Runtime.Port = browser.runtime.connect({
   name: OFFSCREEN_SCRIPT,
 });
 console.log('newConnection2: ', connection);
 
-connection.onMessage.addListener((param) => {
+connection.onMessage.addListener(async (param) => {
   console.log('param: ', param);
+  const params = JSON.parse(param);
+  console.log('params: ', params);
+  const sdk = new GaslessSdk({
+    gasStationUrl: 'https://core-gas-station.avax-test.network',
+  });
+  const isGaslessAvailable = await sdk.isEligibleForChain('1');
+  const isGaslessAvailable2 = await sdk.isEligibleForChain('43114');
+  console.log('isGaslessAvailable: ', isGaslessAvailable);
+  console.log('isGaslessAvailable2: ', isGaslessAvailable2);
 });
 
 connection.postMessage('welcome from offscreen message');
