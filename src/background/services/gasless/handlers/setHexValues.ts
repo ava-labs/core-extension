9d410df017ad49da21a25e9bc5724b7cf9f99bec
Viktor Vasas <viktor.vasas@avalabs.org>
2025-03-18 19:21:35 +0100

    refactor: remove unnecesarry test code from sethexvalues

--- src/background/services/gasless/handlers/setHexValues.ts
+++ src/background/services/gasless/handlers/setHexValues.ts
@@ -20,23 +20,13 @@ export class SetGaslessHexValues implements HandlerType {
 
   constructor(private gasStationService: GasStationService) {}
 
-  // TODO: this will be removed last
-  // this is an example for a retry error message
-  // e5bb36325ad177ebfda7737ed0ec9caa15eca82a353ad1145c5fc1a0f11df165
   handle: HandlerType['handle'] = async ({ request }) => {
     try {
       const { challengeHex, solutionHex, pipelineIndex } = request.params;
       await this.gasStationService.setHexValuesAndFund(
         challengeHex,
         solutionHex,
-        // TODO: remove these comment they can simulate the RETRY and DO NOT RETRY errors at the moment
-        // 'fail',
-        // pipelineIndex === 0
-        //   ? solutionHex
-        // : // : 'fail',
-        // 'e5bb36325ad177ebfda7737ed0ec9caa15eca82a353ad1145c5fc1a0f11df165',
         pipelineIndex ?? undefined,
-        // 'e5bb36325ad177ebfda7737ed0ec9caa15eca82a353ad1145c5fc1a0f11df165',
       );
       return {
         ...request,
