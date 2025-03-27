94a8519d1da143ed4c77b82e3e9075e5df2bf92e
Gergely Lovas <gergely.lovas@avalabs.org>
2025-03-27 13:51:47 +0100

    fix: hide fee message when fee turned off

--- src/pages/Swap/components/TransactionDetails.tsx
+++ src/pages/Swap/components/TransactionDetails.tsx
@@ -14,6 +14,8 @@ import {
 } from '@avalabs/core-k2-components';
 import { PARASWAP_PARTNER_FEE_BPS } from '@src/contexts/SwapProvider/constants';
 import { formatBasisPointsToPercentage } from '../utils';
+import { useFeatureFlagContext } from '@src/contexts/FeatureFlagsProvider';
+import { FeatureGates } from '@src/background/services/featureFlags/models';
 
 interface TransactionDetailsProps {
   fromTokenSymbol: string;
@@ -62,6 +64,7 @@ export function TransactionDetails({
   isTransactionDetailsOpen,
 }: TransactionDetailsProps) {
   const { t } = useTranslation();
+  const { isFlagEnabled } = useFeatureFlagContext();
   const [isDetailsOpen, setIsDetailsOpen] = useState(
     isTransactionDetailsOpen || false,
   );
@@ -154,36 +157,38 @@ export function TransactionDetails({
               />
             </Stack>
           </Stack>
-          <DetailsRow
-            sx={{
-              mt: 2,
-              justifyContent: 'start',
-            }}
-          >
-            <Typography variant="caption" color="text.secondary">
-              {t('Quote includes an {{formattedFeePercent}} Core fee', {
-                formattedFeePercent: formatBasisPointsToPercentage(
-                  PARASWAP_PARTNER_FEE_BPS,
-                ),
-              })}
-            </Typography>
-            <Tooltip
+          {isFlagEnabled(FeatureGates.SWAP_FEES) && (
+            <DetailsRow
               sx={{
-                px: 1,
+                mt: 2,
+                justifyContent: 'start',
               }}
-              isInfo
-              title={t(
-                'Core always finds the best price from the top liquidity providers. A fee of {{formattedFeePercent}} is automatically factored into this quote.',
-                {
+            >
+              <Typography variant="caption" color="text.secondary">
+                {t('Quote includes a {{formattedFeePercent}} Core fee', {
                   formattedFeePercent: formatBasisPointsToPercentage(
                     PARASWAP_PARTNER_FEE_BPS,
                   ),
-                },
-              )}
-            >
-              <InfoCircleIcon sx={{ color: 'text.secondary' }} />
-            </Tooltip>
-          </DetailsRow>
+                })}
+              </Typography>
+              <Tooltip
+                sx={{
+                  px: 1,
+                }}
+                isInfo
+                title={t(
+                  'Core always finds the best price from the top liquidity providers. A fee of {{formattedFeePercent}} is automatically factored into this quote.',
+                  {
+                    formattedFeePercent: formatBasisPointsToPercentage(
+                      PARASWAP_PARTNER_FEE_BPS,
+                    ),
+                  },
+                )}
+              >
+                <InfoCircleIcon sx={{ color: 'text.secondary' }} />
+              </Tooltip>
+            </DetailsRow>
+          )}
         </DetailsContainer>
       </Grow>
     </Container>
