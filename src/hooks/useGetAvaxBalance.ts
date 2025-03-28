eaf3fce498dff96bc6c4b0ef98939ed71fe03d83
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-03-28 11:00:13 +0100

    refactor: getAvaxBalance -> getNativeBalance

--- src/hooks/useGetAvaxBalance.ts
+++ src/hooks/useGetAvaxBalance.ts
@@ -1,13 +1,13 @@
-import { GetAvaxBalanceHandler } from '@src/background/services/balances/handlers/getAvaxBalance';
+import { GetNativeBalanceHandler } from '@src/background/services/balances/handlers/getNativeBalance';
 import { ExtensionRequest } from '@src/background/connections/extensionConnection/models';
 import { useConnectionContext } from '@src/contexts/ConnectionProvider';
 
 export function useGetAvaxBalance() {
   const { request } = useConnectionContext();
   async function getAvaxBalance(address: string) {
-    const result = await request<GetAvaxBalanceHandler>({
-      method: ExtensionRequest.BALANCE_AVAX_GET,
-      params: [address],
+    const result = await request<GetNativeBalanceHandler>({
+      method: ExtensionRequest.BALANCE_NATIVE_GET,
+      params: [address, 'eip155:43114'],
     });
     return result;
   }
