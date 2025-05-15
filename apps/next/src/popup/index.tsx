e3e9e809073ad1d18a3d0ab06dec021423ca9f67
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-05-15 11:33:21 +0200

    fix: initiate i18n

--- apps/next/src/popup/index.tsx
+++ apps/next/src/popup/index.tsx
@@ -15,8 +15,10 @@ import {
 import { lazy, Suspense } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { HashRouter as Router } from 'react-router-dom';
+import { initI18n, i18next } from '@core/common';
 
-import i18n from '@/localization/init';
+// Initialize translations
+initI18n();
 
 const App = lazy(() => {
   return import(/* webpackChunkName: 'App'  */ './app').then((m) => ({
@@ -31,7 +33,7 @@ browser.tabs.query({ active: true }).then(() => {
     <Sentry.ErrorBoundary>
       <Router>
         <ThemeProvider theme="light">
-          <I18nextProvider i18n={i18n}>
+          <I18nextProvider i18n={i18next}>
             <ConnectionContextProvider LoadingComponent={CircularProgress}>
               <SettingsContextProvider>
                 <FeatureFlagsContextProvider>
