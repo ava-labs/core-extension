869c97208fcecb401339efc859f124c16ca4cd50
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-06-06 14:22:53 +0200

    refactor: change notation to allow for ts-expect-error comment

--- apps/next/src/components/OnboardingModal/FullscreenModal.tsx
+++ apps/next/src/components/OnboardingModal/FullscreenModal.tsx
@@ -1,19 +1,21 @@
 import { Dialog, getHexAlpha, styled } from '@avalabs/k2-alpine';
 
-export const FullscreenModal = styled(Dialog)`
-  .MuiPaper-root {
-    padding: 0;
-    width: 100%;
-    max-width: 600px;
-    height: 80vh;
-    max-height: 720px;
-    border-radius: ${({ theme }) => theme.shape.borderRadius * 3}px;
-    background-color: ${({ theme }) => theme.palette.surface.secondary};
-    border-style: solid;
-    border-width: 1px;
-    border-color: ${({ theme }) => getHexAlpha(theme.palette.primary.main, 10)};
-    box-shadow: 0px 15px 30px 0px rgba(0, 0, 0, 0.1);
-    align-items: center;
-    justify-content: center;
-  }
-`;
+export const FullscreenModal = styled(Dialog)(({ theme }) => ({
+  '& .MuiPaper-root': {
+    padding: 0,
+    width: '100%',
+    maxWidth: '600px',
+    height: '80vh',
+    maxHeight: '720px',
+    // @ts-expect-error - Broken Theme type in @avalabs/k2-alpine
+    borderRadius: theme.shape.largeBorderRadius,
+    // @ts-expect-error - Broken Theme type in @avalabs/k2-alpine
+    backgroundColor: theme.palette.surface.secondary,
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderColor: getHexAlpha(theme.palette.primary.main, 10),
+    boxShadow: '0px 15px 30px 0px rgba(0, 0, 0, 0.1)',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+}));
