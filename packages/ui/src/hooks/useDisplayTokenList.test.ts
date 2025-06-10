3989ba5e0953156cf2e37422b90f5e5ee35809d2
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-06-10 19:23:24 +0200

    fix: typings

--- packages/ui/src/hooks/useDisplayTokenList.test.ts
+++ packages/ui/src/hooks/useDisplayTokenList.test.ts
@@ -1,94 +1,154 @@
 import { act } from 'react-dom/test-utils';
 import { useDisplayTokenList } from './useDisplayTokenList';
 import { renderHook } from '@testing-library/react-hooks';
-import { TokenWithBalance } from '@avalabs/vm-module-types';
+import { TokenType, TokenWithBalance } from '@avalabs/vm-module-types';
 
-const TOKENS = [
-  {
-    name: 'Avalanche',
-    symbol: 'AVAX',
-    balance: 5759363806986223209n,
-    balanceInCurrency: 139.77,
-    type: 'NATIVE',
-  },
-  {
-    name: 'YAY Games',
-    symbol: 'YAY',
-    balance: 35000000000000000000n,
-    balanceInCurrency: 0.02,
-    type: 'ERC20',
-  },
-  {
-    name: 'Bitcoin',
-    symbol: 'BTC.b',
-    balance: 16434n,
-    balanceInCurrency: 16.84,
-    type: 'ERC20',
-  },
-  {
-    name: 'YetiSwap',
-    symbol: 'YTS',
-    balance: 16999998305799245218430n,
-    balanceInCurrency: 13.57,
-    type: 'ERC20',
-  },
-  {
-    name: 'Wrapped ETH',
-    symbol: 'WETH.e',
-    balance: 206776540386356860n,
-    balanceInCurrency: 500.8,
-    type: 'ERC20',
-  },
-  {
-    name: 'Jewels',
-    symbol: 'JEWEL',
-    balance: 33982989227679224842n,
-    balanceInCurrency: 1.62,
-    type: 'ERC20',
-  },
-  {
-    name: 'Dexalot',
-    symbol: 'ALOT',
-    balance: 33982989227679224842n,
-    balanceInCurrency: 1.62,
-    type: 'ERC20',
-  },
-  {
-    name: 'LPTokenINSUR B',
-    symbol: 'LPTokenINSUR B',
-    balance: 5000n,
-    reputation: 'Benign',
-    type: 'ERC20',
-  },
-  {
-    name: 'LPTokenINSUR A',
-    symbol: 'LPTokenINSUR A',
-    balance: 10000000n,
-    reputation: 'Benign',
-    type: 'ERC20',
-  },
-  {
-    name: 'ChainLink Token',
-    symbol: 'LINK.e',
-    balance: 598743016848865911n,
-    balanceInCurrency: 9.82,
-    type: 'ERC20',
-  },
-  {
-    name: 'Pangolin',
-    symbol: 'PNG',
-    balance: 16219781798102139248n,
-    balanceInCurrency: 9.82,
-    type: 'ERC20',
-  },
-  {
-    name: 'USD Coin',
-    symbol: 'USDC.e',
-    balance: 1728233n,
-    balanceInCurrency: 1.72,
-    type: 'ERC20',
-  },
-] as unknown as TokenWithBalance[];
+const AVAX: TokenWithBalance = {
+  name: 'Avalanche',
+  symbol: 'AVAX',
+  balance: 5759363806986223209n,
+  balanceInCurrency: 139.77,
+  type: TokenType.NATIVE,
+  balanceDisplayValue: '5.75.77',
+  balancePerType: {},
+  decimals: 18,
+  coingeckoId: 'avalanche-2',
+};
+const YAY_GAMES: TokenWithBalance = {
+  name: 'YAY Games',
+  symbol: 'YAY',
+  balance: 35000000000000000000n,
+  balanceInCurrency: 0.02,
+  type: TokenType.ERC20,
+  address: '0x0000000000000000000000000000000000000000',
+  balanceDisplayValue: '0.35',
+  decimals: 18,
+  reputation: null,
+};
+const BTC_B: TokenWithBalance = {
+  name: 'Bitcoin',
+  symbol: 'BTC.b',
+  balance: 16434n,
+  balanceInCurrency: 16.84,
+  type: TokenType.ERC20,
+  address: '0x0000000000000000000000000000000000000000',
+  balanceDisplayValue: '0.000016434',
+  decimals: 18,
+  reputation: null,
+};
+const YETISWAP: TokenWithBalance = {
+  name: 'YetiSwap',
+  symbol: 'YTS',
+  balance: 16999998305799245218430n,
+  balanceInCurrency: 13.57,
+  type: TokenType.ERC20,
+  address: '0x0000000000000000000000000000000000000000',
+  balanceDisplayValue: '0.016',
+  decimals: 18,
+  reputation: null,
+};
+const WRAPPED_ETH: TokenWithBalance = {
+  name: 'Wrapped ETH',
+  symbol: 'WETH.e',
+  balance: 206776540386356860n,
+  balanceInCurrency: 500.8,
+  type: TokenType.ERC20,
+  address: '0x0000000000000000000000000000000000000000',
+  balanceDisplayValue: '0.2',
+  decimals: 18,
+  reputation: null,
+};
+const JEWELS: TokenWithBalance = {
+  name: 'Jewels',
+  symbol: 'JEWEL',
+  balance: 33982989227679224842n,
+  balanceInCurrency: 1.62,
+  type: TokenType.ERC20,
+  address: '0x0000000000000000000000000000000000000000',
+  balanceDisplayValue: '3.3',
+  decimals: 18,
+  reputation: null,
+};
+const DEXALOT: TokenWithBalance = {
+  name: 'Dexalot',
+  symbol: 'ALOT',
+  balance: 33982989227679224842n,
+  balanceInCurrency: 1.62,
+  type: TokenType.ERC20,
+  address: '0x0000000000000000000000000000000000000000',
+  balanceDisplayValue: '3.3',
+  decimals: 18,
+  reputation: null,
+};
+const LP_INSUR_B: TokenWithBalance = {
+  name: 'LPTokenINSUR B',
+  symbol: 'LPTokenINSUR B',
+  balance: 5000n,
+  type: TokenType.ERC20,
+  address: '0x0000000000000000000000000000000000000000',
+  balanceDisplayValue: '0.05',
+  decimals: 18,
+  reputation: null,
+};
+const LP_INSUR_A: TokenWithBalance = {
+  name: 'LPTokenINSUR A',
+  symbol: 'LPTokenINSUR A',
+  balance: 10000000n,
+  type: TokenType.ERC20,
+  address: '0x0000000000000000000000000000000000000000',
+  balanceDisplayValue: '1',
+  decimals: 18,
+  reputation: null,
+};
+const CHAINLINK: TokenWithBalance = {
+  name: 'ChainLink Token',
+  symbol: 'LINK.e',
+  balance: 598743016848865911n,
+  balanceInCurrency: 9.82,
+  type: TokenType.ERC20,
+
+  address: '0x0000000000000000000000000000000000000000',
+  balanceDisplayValue: '59.87',
+  decimals: 18,
+  reputation: null,
+};
+const PANGOLIN: TokenWithBalance = {
+  name: 'Pangolin',
+  symbol: 'PNG',
+  balance: 16219781798102139248n,
+  balanceInCurrency: 9.82,
+  type: TokenType.ERC20,
+  address: '0x0000000000000000000000000000000000000000',
+  balanceDisplayValue: '1.62',
+  decimals: 18,
+  reputation: null,
+};
+const USDC: TokenWithBalance = {
+  name: 'USD Coin',
+  symbol: 'USDC.e',
+  balance: 1728233n,
+  balanceInCurrency: 1.72,
+  type: TokenType.ERC20,
+  address: '0x0000000000000000000000000000000000000000',
+  balanceDisplayValue: '1.72',
+  decimals: 18,
+  reputation: null,
+};
+
+const TOKENS: TokenWithBalance[] = [
+  AVAX,
+  YAY_GAMES,
+  BTC_B,
+  YETISWAP,
+  WRAPPED_ETH,
+  JEWELS,
+  DEXALOT,
+  LP_INSUR_A,
+  LP_INSUR_B,
+  CHAINLINK,
+  PANGOLIN,
+  USDC,
+];
 
 describe('sortTokens', () => {
   it('properly sorts tokens', async () => {
@@ -102,94 +162,22 @@ describe('sortTokens', () => {
     await act(async () => {
       expect(result.current.map(({ token }) => token)).toEqual([
         // native token first
-        {
-          name: 'Avalanche',
-          symbol: 'AVAX',
-          balance: 5759363806986223209n,
-          balanceInCurrency: 139.77,
-          type: 'NATIVE',
-        },
+        AVAX,
         // then tokens with the highest fiat value
-        {
-          name: 'Wrapped ETH',
-          symbol: 'WETH.e',
-          balance: 206776540386356860n,
-          balanceInCurrency: 500.8,
-          type: 'ERC20',
-        },
-        {
-          name: 'Bitcoin',
-          symbol: 'BTC.b',
-          balance: 16434n,
-          balanceInCurrency: 16.84,
-          type: 'ERC20',
-        },
-        {
-          name: 'YetiSwap',
-          symbol: 'YTS',
-          balance: 16999998305799245218430n,
-          balanceInCurrency: 13.57,
-          type: 'ERC20',
-        },
+        WRAPPED_ETH,
+        BTC_B,
+        YETISWAP,
         // if fiat value is the same, then sort by balance (desc)
-        {
-          name: 'Pangolin',
-          symbol: 'PNG',
-          balance: 16219781798102139248n,
-          balanceInCurrency: 9.82,
-          type: 'ERC20',
-        },
-        {
-          name: 'ChainLink Token',
-          symbol: 'LINK.e',
-          balance: 598743016848865911n,
-          balanceInCurrency: 9.82,
-          type: 'ERC20',
-        },
-        {
-          name: 'USD Coin',
-          symbol: 'USDC.e',
-          balance: 1728233n,
-          balanceInCurrency: 1.72,
-          type: 'ERC20',
-        },
+        PANGOLIN,
+        CHAINLINK,
+        USDC,
         // if balance is the same, then sort by name (asc)
-        {
-          name: 'Dexalot',
-          symbol: 'ALOT',
-          balance: 33982989227679224842n,
-          balanceInCurrency: 1.62,
-          type: 'ERC20',
-        },
-        {
-          name: 'Jewels',
-          symbol: 'JEWEL',
-          balance: 33982989227679224842n,
-          balanceInCurrency: 1.62,
-          type: 'ERC20',
-        },
-        {
-          name: 'YAY Games',
-          symbol: 'YAY',
-          balance: 35000000000000000000n,
-          balanceInCurrency: 0.02,
-          type: 'ERC20',
-        },
+        DEXALOT,
+        JEWELS,
+        YAY_GAMES,
         // tokens with unknown fiat value come last
-        {
-          name: 'LPTokenINSUR A',
-          symbol: 'LPTokenINSUR A',
-          balance: 10000000n,
-          reputation: 'Benign',
-          type: 'ERC20',
-        },
-        {
-          name: 'LPTokenINSUR B',
-          symbol: 'LPTokenINSUR B',
-          balance: 5000n,
-          reputation: 'Benign',
-          type: 'ERC20',
-        },
+        LP_INSUR_A,
+        LP_INSUR_B,
       ]);
     });
   });
