70f26c69581d903c495a29d14b6e14bc580d57fe
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-06-13 11:35:53 +0200

    fix: rsbuild configs

--- apps/next/rsbuild.next.alpha.ts
+++ apps/next/rsbuild.next.alpha.ts
@@ -1,35 +1,38 @@
-import { mergeRsbuildConfig } from '@rsbuild/core';
+import { defineConfig, mergeRsbuildConfig } from '@rsbuild/core';
 import { CopyRspackPlugin } from '@rspack/core';
 import commonConfig from './rsbuild.next.common.ts';
 import { transformManifestFiles } from '../../build-scripts/manifestHelpers.js';
 import { getEnvVars } from '../../build-scripts/getEnvVars.ts';
 
-export default mergeRsbuildConfig(commonConfig, {
-  mode: 'production',
-  source: {
-    define: getEnvVars('production'),
-  },
-  tools: {
-    rspack: {
-      plugins: [
-        new CopyRspackPlugin({
-          patterns: [
-            {
-              context: 'manifest/',
-              from: '**/*.json',
-              to: './',
-              transform: transformManifestFiles({
-                name: '[alpha] Core NextGen',
-                shortName: '[alpha] Core NextGen',
-                actionDefaultTitle: '[alpha] Core NextGen Browser Extension',
-                oAuthClientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
-              }),
-              force: true,
-            },
-            { from: 'src/images/beta-logos', to: './images', force: true },
-          ],
-        }),
-      ],
+export default defineConfig((...args) =>
+  mergeRsbuildConfig(commonConfig(...args), {
+    mode: 'production',
+    source: {
+      define: getEnvVars('production'),
     },
-  },
-});
+    tools: {
+      rspack: {
+        plugins: [
+          new CopyRspackPlugin({
+            patterns: [
+              {
+                context: 'manifest/',
+                from: '**/*.json',
+                to: './',
+                transform: transformManifestFiles({
+                  name: '[alpha] Core NextGen',
+                  shortName: '[alpha] Core NextGen',
+                  actionDefaultTitle: '[alpha] Core NextGen Browser Extension',
+                  oAuthClientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
+                  publicKey: process.env.NEXT_GEN_EXTENSION_PUBLIC_KEY,
+                }),
+                force: true,
+              },
+              { from: 'src/images/beta-logos', to: './images', force: true },
+            ],
+          }),
+        ],
+      },
+    },
+  }),
+);
