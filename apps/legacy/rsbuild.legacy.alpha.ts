55f4c2f87f3063651047ebb5a3daf888c9838909
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-06-13 11:35:53 +0200

    fix: rsbuild configs

--- apps/legacy/rsbuild.legacy.alpha.ts
+++ apps/legacy/rsbuild.legacy.alpha.ts
@@ -1,4 +1,4 @@
-import { mergeRsbuildConfig } from '@rsbuild/core';
+import { defineConfig, mergeRsbuildConfig } from '@rsbuild/core';
 import { CopyRspackPlugin } from '@rspack/core';
 import commonConfig from './rsbuild.legacy.common.ts';
 import { transformManifestFiles } from '../../build-scripts/manifestHelpers.js';
@@ -6,37 +6,39 @@ import { getEnvVars } from '../../build-scripts/getEnvVars.ts';
 
 const skipSourceMap = process.env.NO_SOURCE_MAPS === 'true';
 
-export default mergeRsbuildConfig(commonConfig, {
-  mode: 'production',
-  output: {
-    sourceMap: skipSourceMap ? false : { js: 'hidden-source-map' },
-  },
-  source: {
-    define: getEnvVars('production'),
-  },
-  tools: {
-    rspack: {
-      plugins: [
-        new CopyRspackPlugin({
-          patterns: [
-            {
-              context: 'manifest/',
-              from: '**/*.json',
-              to: './',
-              transform: transformManifestFiles({
-                name: 'Core Beta DEVELOPMENT BUILD',
-                shortName: 'Core Beta',
-                actionDefaultTitle:
-                  'Core Beta Browser Extension DEVELOPMENT BUILD',
-                oAuthClientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
-                publicKey: process.env.EXTENSION_PUBLIC_KEY,
-              }),
-              force: true,
-            },
-            { from: 'src/images/beta-logos', to: './images', force: true },
-          ],
-        }),
-      ],
+export default defineConfig((...args) =>
+  mergeRsbuildConfig(commonConfig(...args), {
+    mode: 'production',
+    output: {
+      sourceMap: skipSourceMap ? false : { js: 'hidden-source-map' },
     },
-  },
-});
+    source: {
+      define: getEnvVars('production'),
+    },
+    tools: {
+      rspack: {
+        plugins: [
+          new CopyRspackPlugin({
+            patterns: [
+              {
+                context: 'manifest/',
+                from: '**/*.json',
+                to: './',
+                transform: transformManifestFiles({
+                  name: 'Core Beta DEVELOPMENT BUILD',
+                  shortName: 'Core Beta',
+                  actionDefaultTitle:
+                    'Core Beta Browser Extension DEVELOPMENT BUILD',
+                  oAuthClientId: process.env.GOOGLE_OAUTH_CLIENT_ID,
+                  publicKey: process.env.EXTENSION_PUBLIC_KEY,
+                }),
+                force: true,
+              },
+              { from: 'src/images/beta-logos', to: './images', force: true },
+            ],
+          }),
+        ],
+      },
+    },
+  }),
+);
