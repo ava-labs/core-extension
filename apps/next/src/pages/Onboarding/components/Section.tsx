ba7e8682253defc5c750da3f3fca8040d9532e19
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-06-16 10:59:26 +0200

    chore: update sdks & k2-alpine

--- apps/next/src/pages/Onboarding/components/Section.tsx
+++ apps/next/src/pages/Onboarding/components/Section.tsx
@@ -13,7 +13,6 @@ export const Section: FC<StackProps> = ({ sx, ...props }) => {
       sx={combineSx(
         {
           backgroundColor: 'background.paper',
-          // @ts-expect-error - Broken Theme type in @avalabs/k2-alpine
           borderRadius: theme.shape.mediumBorderRadius,
           py: 0.75,
           px: 2,
