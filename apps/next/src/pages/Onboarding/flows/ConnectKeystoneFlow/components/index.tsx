4ce3102ae99ca0142026cf1e87a4321cd4e0c24b
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-07-11 15:32:40 +0200

    feat(next-gen): onboarding with keystone (via USB)

--- apps/next/src/pages/Onboarding/flows/ConnectKeystoneFlow/components/index.tsx
+++ apps/next/src/pages/Onboarding/flows/ConnectKeystoneFlow/components/index.tsx
@@ -1,1 +1,5 @@
 export * from './KeystoneQRConnector';
+export * from './KeystoneUSBConnector';
+export * from './KeystoneDeviceSelect';
+export * from './KeystonQRInstructions';
+export * from './Styled';
