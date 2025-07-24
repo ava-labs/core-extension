a9c6a09ee33e01d98d9ccd6ffd544fc87ca97fc2
Bartosz Nowak <bartosz.nowak@avalabs.org>
2025-07-24 18:27:25 +0200

    feat(next-gen): on ramp
    fix(next-gen): cleanup css
    
    feat(next-gen): on ramp navigation

--- apps/next/src/pages/Portfolio/components/OnRampForm/components/AvaGradient.tsx
+++ apps/next/src/pages/Portfolio/components/OnRampForm/components/AvaGradient.tsx
@@ -1,15 +1,49 @@
+import AvaDark from '@/images/on-ramp-gradients/ava.dark.svg';
 import AvaLight from '@/images/on-ramp-gradients/ava.light.svg';
-import { styled } from '@avalabs/k2-alpine';
+import { styled, Theme } from '@avalabs/k2-alpine';
+import { CSSProperties, useState } from 'react';
 
-const Image = styled('div')({
-  backgroundImage: `url(${AvaLight.src})`,
-  bac,
-});
+const isLightTheme = (theme: Theme) => theme.palette.mode === 'light';
+
+const Image = styled('div')(({ theme }) => ({
+  '--spot-size': '40px',
+  '--spot-color': isLightTheme(theme)
+    ? theme.palette.common.white_30
+    : theme.palette.common.black_25,
+
+  position: 'relative',
+  width: '100%',
+  height: '100%',
+  backgroundImage: `url(${isLightTheme(theme) ? AvaLight : AvaDark})`,
+  backgroundSize: 'auto',
+  backgroundPosition: 'bottom right',
+  backgroundRepeat: 'no-repeat',
+  overflow: 'hidden',
+
+  '&:hover::before': {
+    content: '""',
+    background: 'inherit',
+    position: 'absolute',
+    inset: 0,
+    mask: `radial-gradient(circle var(--spot-size) at var(--x) var(--y), var(--spot-color) 0%, var(--spot-color) 30%, transparent 100%)`,
+    mixBlendMode: isLightTheme(theme) ? 'difference' : 'plus-lighter',
+  },
+}));
 
 export const AvaGradient = () => {
+  const [x, setX] = useState<string>('85px');
+  const [y, setY] = useState<string>('75px');
+
   return (
-    <>
-      <img src="/images/on-ramp-gradients/ava.light.svg" alt="Ava Gradient" />
-    </>
+    <Image
+      style={{ '--x': x, '--y': y } as CSSProperties}
+      onMouseMove={(event) => {
+        const rect = event.currentTarget.getBoundingClientRect();
+        const relativeX = event.clientX - rect.left;
+        const relativeY = event.clientY - rect.top;
+        setX(`${relativeX}px`);
+        setY(`${relativeY}px`);
+      }}
+    />
   );
 };
