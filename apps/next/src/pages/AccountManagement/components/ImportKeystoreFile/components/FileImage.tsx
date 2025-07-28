daa0c2a79e74cb181291c3a462b9e99ffbceae43
Fumiko <fumikom@gmail.com>
2025-07-28 13:42:45 -0600

    fix(import): refactor

--- apps/next/src/pages/AccountManagement/components/ImportKeystoreFile/components/FileImage.tsx
+++ apps/next/src/pages/AccountManagement/components/ImportKeystoreFile/components/FileImage.tsx
@@ -1,13 +1,13 @@
 import { useTheme } from '@avalabs/k2-alpine';
-import FileImageLight from '../assets/lightFileImage.png';
-import FileImageDark from '../assets/darkFileImage.png';
+import DarkImage from '../assets/darkFileImage.svg';
+import LightImage from '../assets/lightFileImage.svg';
 
 const Light = () => {
-  return <img src={FileImageLight} alt="File Image" width={44} height={51} />;
+  return <img src={LightImage} alt="File Image" width={44} height={51} />;
 };
 
 const Dark = () => {
-  return <img src={FileImageDark} alt="File Image" width={44} height={51} />;
+  return <img src={DarkImage} alt="File Image" width={44} height={51} />;
 };
 
 export const FileImage = () => {
