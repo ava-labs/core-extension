2a88abe57669d76cfeecf05a3f1091f5af484d6a
Bartosz Nowak <bartosz.nowak@avalabs.org>
2025-07-30 10:22:06 +0200

    feat(next-gen): rename OnRampForm to EmptyState

--- apps/next/src/pages/Portfolio/Portfolio.tsx
+++ apps/next/src/pages/Portfolio/Portfolio.tsx
@@ -1,27 +1,32 @@
-import { Box, Stack } from '@avalabs/k2-alpine';
+import { CircularProgress, Stack, styled } from '@avalabs/k2-alpine';
 import { useAccountsContext, useBalancesContext } from '@core/ui';
 import { FC, useState } from 'react';
 import AccountInfo from './components/AccountInfo';
+import { EmptyState } from './components/EmptyState';
 import NavigationBar, { TabName } from './components/NavigationBar';
-import { OnRampForm } from './components/OnRampForm';
 import { PortfolioDetails } from './components/PortolioDetails';
 
 export const Portfolio: FC = () => {
   const { accounts } = useAccountsContext();
   const [activeTab, setActiveTab] = useState<TabName>('assets');
   const { totalBalance } = useBalancesContext();
-  const isAccountEmpty = totalBalance?.sum === 0;
-  const PortfolioPage = isAccountEmpty ? OnRampForm : PortfolioDetails;
+  const isLoading = !totalBalance;
+  const isAccountEmpty = !isLoading && totalBalance.sum === 0;
+  const PortfolioPage = isAccountEmpty ? EmptyState : PortfolioDetails;
   return (
     <Stack height={1} px={1.5} pb={1.5} gap={2.5}>
       <AccountInfo
         accountName={accounts.active?.name ?? ''}
         balance={totalBalance}
       />
-      <Box flexGrow={1}>
-        <PortfolioPage />
-      </Box>
+      <Stack flexGrow={1}>
+        {isLoading ? <CenteredSpinner /> : <PortfolioPage tab={activeTab} />}
+      </Stack>
       <NavigationBar active={activeTab} onChange={setActiveTab} />
     </Stack>
   );
 };
+
+const CenteredSpinner = styled(CircularProgress)({
+  margin: 'auto',
+});
