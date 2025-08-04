7b5438e3a209523539767333fca1d965b2ac61b1
Bartosz Nowak <bartosz.nowak@avalabs.org>
2025-08-04 20:48:15 +0200

    feat(next-gen): better progressarc

---
+++ apps/next/src/components/ArcProgress.tsx
@@ -0,0 +1,67 @@
+import { Theme, useTheme } from '@avalabs/core-k2-components';
+import { get } from 'lodash';
+
+type Props = {
+  size: number;
+  value: number; // from 0 to 100
+  thickness?: number;
+  bgColor?: string;
+  color?: string;
+};
+
+const resolveColor = (theme: Theme, color: string) => {
+  const resolved = get(theme, `palette.${color}`, color);
+  if (typeof resolved === 'string') {
+    return resolved;
+  }
+
+  return resolved.main;
+};
+
+export const ArcProgress = ({
+  size,
+  value,
+  thickness = 5,
+  bgColor = 'grey.800',
+  color = 'secondary',
+}: Props) => {
+  const theme = useTheme();
+
+  const width = size;
+  const height = size / 2;
+  const xStart = thickness;
+  const xEnd = xStart + width;
+  const y = height + thickness;
+  const radius = (width - thickness) / 2;
+
+  // Path of the arc to draw:
+  const d = `M ${thickness} ${y} A ${radius} ${radius} 180 0 1 ${xEnd} ${y}`;
+
+  // Since the arc is coded to always be exactly half of the circle,
+  // we can calculate the % values based on a formula for circumference of a circle.
+  const circumference = 2 * Math.PI * (size / 2);
+  const progressValue = ((100 - value) / 100) * circumference;
+
+  return (
+    <svg
+      width={size + 2 * thickness}
+      height={size / 2 + 2 * thickness}
+      strokeLinecap="round"
+      fill="none"
+    >
+      <path
+        d={d}
+        strokeWidth={thickness}
+        stroke={resolveColor(theme, bgColor)}
+      />
+      <path
+        d={d}
+        strokeWidth={thickness}
+        stroke={resolveColor(theme, color)}
+        strokeDasharray={circumference / 2}
+        strokeDashoffset={progressValue / 2}
+        style={{ transition: 'stroke-dashoffset .15s ease-in-out' }}
+      />
+    </svg>
+  );
+};
