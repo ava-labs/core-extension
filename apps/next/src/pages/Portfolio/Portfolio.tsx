679d89f28ec8be08ccb219b7cac78b82340e263f
Viktor Vasas <viktor.vasas@avalabs.org>
2025-08-12 11:01:29 +0200

    feat: add TabBar from k2

--- apps/next/src/pages/Portfolio/Portfolio.tsx
+++ apps/next/src/pages/Portfolio/Portfolio.tsx
@@ -1,18 +1,46 @@
-import { CircularProgress, Stack, styled } from '@avalabs/k2-alpine';
+import {
+  CircularProgress,
+  Stack,
+  styled,
+  TabBar,
+  TabBarItemProps,
+} from '@avalabs/k2-alpine';
 import { useAccountsContext, useBalancesContext } from '@core/ui';
 import { FC, useState } from 'react';
 import AccountInfo from './components/AccountInfo';
 import { EmptyState } from './components/EmptyState';
-import NavigationBar, { TabName } from './components/NavigationBar';
 import { PortfolioDetails } from './components/PortolioDetails';
+import { useTranslation } from 'react-i18next';
+
+export type TabName = 'assets' | 'collectibles' | 'defi' | 'activity';
 
 export const Portfolio: FC = () => {
+  const { t } = useTranslation();
   const { accounts } = useAccountsContext();
   const [activeTab, setActiveTab] = useState<TabName>('assets');
   const { totalBalance } = useBalancesContext();
   const isLoading = !totalBalance;
   const isAccountEmpty = !isLoading && totalBalance.sum === 0;
   const PortfolioContent = isAccountEmpty ? EmptyState : PortfolioDetails;
+
+  const TABS: TabBarItemProps[] = [
+    {
+      id: 'assets',
+      label: t('Assets'),
+    },
+    {
+      id: 'collectibles',
+      label: t('Collectibles'),
+    },
+    {
+      id: 'defi',
+      label: t('DeFi'),
+    },
+    {
+      id: 'activity',
+      label: t('Activity'),
+    },
+  ];
 
   return (
     <Stack height={1} px={1.5} pb={1.5} gap={2.5}>
@@ -23,7 +51,14 @@ export const Portfolio: FC = () => {
       <Stack flexGrow={1} gap={2.5}>
         {isLoading ? <CenteredSpinner /> : <PortfolioContent tab={activeTab} />}
       </Stack>
-      <NavigationBar active={activeTab} onChange={setActiveTab} />
+      <TabBar
+        tabBarItems={TABS}
+        value={activeTab}
+        onChange={(_, val) => {
+          setActiveTab(val);
+        }}
+        size="extension"
+      />
     </Stack>
   );
 };
