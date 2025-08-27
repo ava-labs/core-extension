0b0f82bf64e6a967e0e3d502db4accd9e18d14f6
Bartosz Nowak <bartosz.nowak@avalabs.org>
2025-08-27 11:42:30 +0200

    chore: portfolio to home

---
+++ apps/next/src/pages/Home/components/PortolioDetails/components/AssetsTab.tsx
@@ -0,0 +1,46 @@
+import { Box, Button, Stack, toast } from '@avalabs/k2-alpine';
+import { FC } from 'react';
+import { useTranslation } from 'react-i18next';
+import { MdCurrencyBitcoin, MdKeyboardArrowDown } from 'react-icons/md';
+import { useHistory } from 'react-router-dom';
+import { UnderConstruction } from './UnderConstruction';
+
+export const AssetsTab: FC = () => {
+  const { t } = useTranslation();
+  const { push } = useHistory();
+  return (
+    <Stack direction="column" gap={1.25} height={1}>
+      <Box height={40} bgcolor="background.paper" borderRadius={2} px={2}>
+        Trending tokens placeholder
+      </Box>
+      <Stack direction="row" gap={1.25}>
+        <Button
+          variant="outlined"
+          size="small"
+          endIcon={<MdKeyboardArrowDown />}
+          onClick={() => toast.info('Coming soon')}
+        >
+          {t('filter')}
+        </Button>
+        <Button
+          variant="outlined"
+          size="small"
+          endIcon={<MdKeyboardArrowDown />}
+          onClick={() => toast.info('Coming soon')}
+        >
+          {t('sort')}
+        </Button>
+        <Box ml="auto">
+          <Button variant="outlined" size="small">
+            {t('manage')}
+          </Button>
+        </Box>
+      </Stack>
+      <UnderConstruction
+        title="Assets"
+        description="Your assets will be displayed here. We're working hard to bring you this feature soon!"
+        icon={<MdCurrencyBitcoin size={24} />}
+      />
+    </Stack>
+  );
+};
