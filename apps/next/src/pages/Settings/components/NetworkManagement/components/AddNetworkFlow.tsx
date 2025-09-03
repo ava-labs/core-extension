369c483e3152320781b3ae946fc43b38175dbbf9
Fumiko <fumikom@gmail.com>
2025-09-03 13:30:46 -0600

    fix(network): add navigations

--- apps/next/src/pages/Settings/components/NetworkManagement/components/AddNetworkFlow.tsx
+++ apps/next/src/pages/Settings/components/NetworkManagement/components/AddNetworkFlow.tsx
@@ -1,3 +1,17 @@
+import { PageTopBar } from '@/components/PageTopBar';
+import { Stack } from '@avalabs/k2-alpine';
+
 export const AddNetworkFlow = () => {
-  return <div>AddNetworkFlow</div>;
+  return (
+    <Stack
+      height="100cqh"
+      width={1}
+      bgcolor="background.backdrop"
+      overflow="hidden"
+      sx={{ position: 'relative' }}
+    >
+      <PageTopBar showBack={true} />
+      <div>AddNetworkFlow</div>;
+    </Stack>
+  );
 };
