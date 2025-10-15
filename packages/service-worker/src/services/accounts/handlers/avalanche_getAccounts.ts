a65106ba6fb84b49de30ad18e3277dbd19dad726
Gergely Lovas <36536513+gergelylovas@users.noreply.github.com>
2025-10-15 17:04:42 +0200

    feat: return xpub with accounts for Core Web CP-12373 (#459)

--- packages/service-worker/src/services/accounts/handlers/avalanche_getAccounts.ts
+++ packages/service-worker/src/services/accounts/handlers/avalanche_getAccounts.ts
@@ -1,12 +1,12 @@
 import {
   DAppRequestHandler,
   DAppProviderRequest,
-  Account,
-  AccountType,
+  AVALANCHE_BASE_DERIVATION_PATH,
 } from '@core/types';
 import { injectable } from 'tsyringe';
 import { AccountsService } from '../AccountsService';
 import { SecretsService } from '../../secrets/SecretsService';
+import { getExtendedPublicKey } from '~/services/secrets/utils';
 
 @injectable()
 export class AvalancheGetAccountsHandler extends DAppRequestHandler {
@@ -20,36 +20,57 @@ export class AvalancheGetAccountsHandler extends DAppRequestHandler {
   }
 
   handleAuthenticated = async ({ request }) => {
-    const accounts = await this.accountsService.getAccountList();
-    const activeAccount = await this.accountsService.getActiveAccount();
-    const wallets = await this.secretsService.getPrimaryWalletsDetails();
+    const accounts = await this.accountsService.getAccounts();
 
-    const getWalletData = (acc: Account) => {
-      if (acc.type === AccountType.PRIMARY) {
-        const walletData = wallets.find((wallet) => {
-          return wallet.id === acc.walletId;
+    const mappedPrimaryAccounts = await Promise.all(
+      Object.keys(accounts.primary).map(async (walletId) => {
+        const walletAccounts = accounts.primary[walletId];
+        if (!walletAccounts) {
+          return [];
+        }
+
+        const secrets = await this.secretsService.getSecretsById(walletId);
+        const xpubXP =
+          'extendedPublicKeys' in secrets
+            ? getExtendedPublicKey(
+                secrets.extendedPublicKeys,
+                AVALANCHE_BASE_DERIVATION_PATH,
+                'secp256k1',
+              )
+            : null;
+
+        return walletAccounts.map((acc) => {
+          const active = accounts.active?.id === acc.id;
+          return {
+            ...acc,
+            walletType: secrets.secretType,
+            walletName: 'name' in secrets ? secrets.name : undefined,
+            xpubXP: xpubXP?.key,
+            active,
+          };
         });
-        return {
-          name: walletData?.name,
-          type: walletData?.type,
-        };
-      }
-      return null;
-    };
+      }),
+    );
 
-    return {
-      ...request,
-      result: accounts.map((acc) => {
-        const active = activeAccount?.id === acc.id;
-        const walletData = getWalletData(acc);
-
+    const mappedImportedAccounts = await Promise.all(
+      Object.values(accounts.imported).map(async (importedAccount) => {
+        const secrets = await this.secretsService.getImportedAccountSecrets(
+          importedAccount.id,
+        );
+        const active = accounts.active?.id === importedAccount.id;
         return {
-          ...acc,
-          walletType: walletData?.type,
-          walletName: walletData?.name,
+          ...importedAccount,
+          walletType: secrets.secretType,
+          walletName: undefined,
+          xpubXP: undefined,
           active,
         };
       }),
+    );
+
+    return {
+      ...request,
+      result: [...mappedPrimaryAccounts.flat(), ...mappedImportedAccounts],
     };
   };
 
