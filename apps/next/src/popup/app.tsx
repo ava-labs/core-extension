951ffa7242b74d8fc6ef78e5da38354219c403e8
Michał Leszczyk <michal.leszczyk@avalabs.org>
2025-10-31 16:29:48 +0100

    feat(ng): ledger BTC wallet policies

--- apps/next/src/popup/app.tsx
+++ apps/next/src/popup/app.tsx
@@ -38,6 +38,7 @@ import { useSwapCallbacks } from '@/pages/Swap';
 import { AppRoutes, ApprovalRoutes } from '@/routing';
 import { Children, ReactElement } from 'react';
 import { Providers } from './providers';
+import { LedgerRegisterBtcWalletPolicy } from '@/components/ledger/LedgerRegisterBtcWalletPolicy';
 
 const pagesWithoutHeader = [
   '/account-management',
@@ -140,6 +141,7 @@ export function App() {
         )}
         {isApprovalContext ? <ApprovalRoutes /> : <AppRoutes />}
         {isAppContext && <InAppApprovalOverlay />}
+        <LedgerRegisterBtcWalletPolicy />
       </>
     </Providers>
   );
