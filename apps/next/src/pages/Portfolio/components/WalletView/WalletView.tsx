4dd57b8726651c82ad48af20817de9869ab2f5ba
Fumiko <fumikom@gmail.com>
2025-11-13 09:04:16 -0700

    feat: wallet view navigation logic

--- apps/next/src/pages/Portfolio/components/WalletView/WalletView.tsx
+++ apps/next/src/pages/Portfolio/components/WalletView/WalletView.tsx
@@ -1,17 +1,41 @@
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import { useAccountsContext, useWalletContext } from '@core/ui';
+import { Stack, Typography } from '@avalabs/k2-alpine';
+import { useCallback } from 'react';
+import { Account } from '@core/types';
+import { MdCheck } from 'react-icons/md';
 
 export const WalletView = () => {
   const { walletId } = useParams<{ walletId: string }>();
+  const history = useHistory();
   const { getWallet } = useWalletContext();
   const { getAccountsByWalletId } = useAccountsContext();
-  const wallet = getWallet(walletId);
-  const accounts = getAccountsByWalletId(walletId);
+  const { selectAccount, isActiveAccount } = useAccountsContext();
 
+  const wallet = getWallet(walletId);
+  const accountsInWallet = getAccountsByWalletId(walletId);
+
+  const clickHandler = useCallback(
+    (account: Account) => {
+      selectAccount(account.id);
+      history.push(`/portfolio`);
+    },
+    [history, selectAccount],
+  );
   return (
     <>
       <div>{wallet?.name}</div>
-      <div>{accounts.map((account) => account.name).join(', ')}</div>
+      <div>
+        {accountsInWallet.map((account) => (
+          <Stack
+            key={`wallet-${account.id}`}
+            onClick={() => clickHandler(account)}
+          >
+            {isActiveAccount(account.id) && <MdCheck />}
+            <Typography>{account.name}</Typography>
+          </Stack>
+        ))}
+      </div>
     </>
   );
 };
