2963a05e4c215ce806770c2b9e43d45f94b9ccf7
Csaba Valyi <csaba.valyi@avalabs.org>
2025-11-21 19:48:08 +0100

    feat: fix broken test

--- src/tests/mockClientApis.ts
+++ src/tests/mockClientApis.ts
@@ -1,5 +1,6 @@
 jest.mock('~/api-clients', () => ({
   profileApiClient: jest.fn(),
+  callGetAddresses: jest.fn(),
 }));
 jest.mock('~/api-clients/profile-api', () => ({
   postV1GetAddresses: jest.fn(),
