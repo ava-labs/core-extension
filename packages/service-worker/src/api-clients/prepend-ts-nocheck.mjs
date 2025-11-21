afd78e50b3e4e280276642d9e253d1540b292248
Csaba Valyi <csaba.valyi@avalabs.org>
2025-11-21 16:35:12 +0100

    feat: create script to add no check

---
+++ packages/service-worker/src/api-clients/prepend-ts-nocheck.mjs
@@ -0,0 +1,26 @@
+import { glob } from 'glob';
+import { dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import fs from 'node:fs';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const filesToModify = ['queryKeySerializer.gen.ts'];
+
+console.log('asdasdasdas', __dirname);
+
+const files = glob.sync(__dirname + '/**/*');
+
+const filteredFiles = files.filter((fileWithPath) =>
+  filesToModify.some((file) => fileWithPath.includes(file)),
+);
+
+filteredFiles.map((file) => {
+  const fileContent = fs.readFileSync(file);
+  const content = fileContent.toString();
+
+  const prepended = '// @ts-nocheck\n' + content;
+
+  fs.writeFileSync(file, Buffer.from(prepended));
+});
