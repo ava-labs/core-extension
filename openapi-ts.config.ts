739341729d86d8fc4f36e5981131d7d62a729d07
Csaba Valyi <csaba.valyi@avalabs.org>
2025-11-24 10:05:14 +0100

    feat: using prod schemas to generate v1 clients

--- openapi-ts.config.ts
+++ openapi-ts.config.ts
@@ -3,11 +3,11 @@ import { defineConfig } from '@hey-api/openapi-ts';
 export default defineConfig([
   // multiple config can go here
   {
-    input: 'https://core-balance-api.avax-test.network/schema.json',
+    input: 'https://core-balance-api.avax.network/schema.json',
     output: 'packages/service-worker/src/api-clients/balance-api',
   },
   {
-    input: 'https://core-profile-api.avax-test.network/schema.json',
+    input: 'https://core-profile-api.avax.network/schema.json',
     output: 'packages/service-worker/src/api-clients/profile-api',
   },
 ]);
