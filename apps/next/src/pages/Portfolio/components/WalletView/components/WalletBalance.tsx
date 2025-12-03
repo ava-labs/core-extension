3486636a8c35f17f04223347569a7c8e55b2b1eb
Fumiko <fumikom@gmail.com>
2025-12-03 15:23:44 -0700

    fix: wallet View fix

--- apps/next/src/pages/Portfolio/components/WalletView/components/WalletBalance.tsx
+++ apps/next/src/pages/Portfolio/components/WalletView/components/WalletBalance.tsx
@@ -6,11 +6,13 @@ import {
   useTheme,
 } from '@avalabs/k2-alpine';
 import { BalanceChange } from '../../BalanceChange';
-import { MdError } from 'react-icons/md';
+import { MdError, MdUnfoldMore } from 'react-icons/md';
 import { Trans } from 'react-i18next';
 import { SecretType, SeedlessAuthProvider } from '@core/types';
 import { useMemo } from 'react';
 import { useSettingsContext } from '@core/ui';
+import { ClickableStack } from '../../PortfolioHome/components/AccountInfo/styled';
+import { useHistory } from 'react-router-dom';
 
 type Props = {
   walletType?: SecretType;
@@ -33,6 +35,7 @@ export const WalletBalance = ({
   percentageChange,
 }: Props) => {
   const theme = useTheme();
+  const history = useHistory();
   const { currencyFormatter, currency } = useSettingsContext();
 
   const walletIconSize = useMemo(() => {
@@ -52,52 +55,67 @@ export const WalletBalance = ({
     [currencyFormatter],
   );
 
-  return (
-    <Stack gap={1} ml={0.5}>
-      <Stack direction="row" alignItems="center" gap={1} color="text.secondary">
-        {walletType && (
-          <WalletIcon
-            size={walletIconSize}
-            type={walletType}
-            authProvider={walletAuthProvider}
-          />
-        )}
+  const balanceToDisplay = useMemo(() => {
+    return isLoading
+      ? placeholderTotalBalance
+      : totalBalanceInCurrency !== undefined
+        ? currencyFormatter(totalBalanceInCurrency)
+        : placeholderTotalBalance;
+  }, [
+    isLoading,
+    totalBalanceInCurrency,
+    placeholderTotalBalance,
+    currencyFormatter,
+  ]);
 
-        <Typography variant="h2">{walletName}</Typography>
-      </Stack>
-      {isLoading && (
-        <Stack height={48.5} justifyContent="flex-start">
-          <Stack direction="row" alignItems="center" gap={1}>
-            <Typography variant="h2">{placeholderTotalBalance}</Typography>
-            <Typography variant="h7">{currency}</Typography>
-            <CircularProgress size={14} />
-          </Stack>
-        </Stack>
-      )}
-      {!isLoading && (
-        <Stack>
-          <Stack direction="row" alignItems="baseline" gap={0.5}>
-            <Typography variant="h2">
-              {totalBalanceInCurrency !== undefined
-                ? currencyFormatter(totalBalanceInCurrency)
-                : placeholderTotalBalance}
-            </Typography>
-            <Typography variant="h7">{currency}</Typography>
-          </Stack>
-          <BalanceChange
-            balanceChange={balanceChange}
-            percentageChange={percentageChange}
-          />
-          {hasErrorOccurred && (
-            <Stack direction="row" alignItems="center" gap={0.5}>
-              <MdError size={20} color={theme.palette.error.main} />
-              <Typography variant="subtitle4" color="error">
-                {
-                  <Trans i18nKey="Unable to load all network balances, <br /> total may be incomplete" />
-                }
-              </Typography>
-            </Stack>
+  return (
+    <Stack ml={0.5} mb={1}>
+      <ClickableStack
+        onClick={() => {
+          history.push('/account-management');
+        }}
+      >
+        <Stack
+          direction="row"
+          alignItems="center"
+          gap={1}
+          color="text.secondary"
+        >
+          {walletType && (
+            <WalletIcon
+              size={walletIconSize}
+              type={walletType}
+              authProvider={walletAuthProvider}
+            />
           )}
+
+          <Typography variant="h2">{walletName}</Typography>
+          <MdUnfoldMore size={16} color={theme.palette.text.secondary} />
+        </Stack>
+        <Stack direction="row" alignItems="baseline" gap={0.5} ml={0.5}>
+          <Typography variant="h2">{balanceToDisplay}</Typography>
+          <Typography variant="h7">{currency}</Typography>
+          {isLoading && (
+            <CircularProgress size={14} sx={{ margin: '0 0 0 8px' }} />
+          )}
+        </Stack>
+      </ClickableStack>
+
+      {!isLoading && (
+        <BalanceChange
+          balanceChange={balanceChange}
+          percentageChange={percentageChange}
+        />
+      )}
+
+      {hasErrorOccurred && (
+        <Stack direction="row" alignItems="center" gap={0.5}>
+          <MdError size={20} color={theme.palette.error.main} />
+          <Typography variant="subtitle4" color="error">
+            {
+              <Trans i18nKey="Unable to load all network balances, <br /> total may be incomplete" />
+            }
+          </Typography>
         </Stack>
       )}
     </Stack>
