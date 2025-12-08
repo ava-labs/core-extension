7a89205c7b986dff23c8915e4040bebda52bf827
Csaba Vályi <csaba.valyi@avalabs.org>
2025-12-08 10:29:12 +0100

    feat: move WalletTotalBalanceProvider to the root level (#544)

--- apps/next/src/pages/AccountManagement/AccountManagement.tsx
+++ apps/next/src/pages/AccountManagement/AccountManagement.tsx
@@ -1,4 +1,4 @@
-import { AccountManagerProvider, WalletTotalBalanceProvider } from '@core/ui';
+import { AccountManagerProvider } from '@core/ui';
 import { FC } from 'react';
 import { Route, Switch, useRouteMatch } from 'react-router-dom';
 import { AccountDetails } from './components/AccountDetails';
@@ -14,27 +14,25 @@ import { WalletsHomePage } from './components/Wallets';
 const AccountManagement: FC = () => {
   const { path } = useRouteMatch();
   return (
-    <WalletTotalBalanceProvider>
-      <AccountManagerProvider>
-        <Switch>
-          <Route path={path} exact component={WalletsHomePage} />
-          <Route path={`${path}/rename`} component={RenamePage} />
-          <Route path={`${path}/delete-account`} component={DeleteAccount} />
-          <Route path={`${path}/qr-code`} component={QRCode} />
-          <Route path={`${path}/add-wallet`} component={AddOrConnectWallet} />
-          <Route
-            path={`${path}/import-private-key`}
-            component={ImportPrivateKey}
-          />
-          <Route
-            path={`${path}/import-keystore-file`}
-            component={ImportKeystoreFile}
-          />
-          <Route path={`${path}/account`} component={AccountDetails} />
-          <Route path={`${path}/show-private-key`} component={ShowPrivateKey} />
-        </Switch>
-      </AccountManagerProvider>
-    </WalletTotalBalanceProvider>
+    <AccountManagerProvider>
+      <Switch>
+        <Route path={path} exact component={WalletsHomePage} />
+        <Route path={`${path}/rename`} component={RenamePage} />
+        <Route path={`${path}/delete-account`} component={DeleteAccount} />
+        <Route path={`${path}/qr-code`} component={QRCode} />
+        <Route path={`${path}/add-wallet`} component={AddOrConnectWallet} />
+        <Route
+          path={`${path}/import-private-key`}
+          component={ImportPrivateKey}
+        />
+        <Route
+          path={`${path}/import-keystore-file`}
+          component={ImportKeystoreFile}
+        />
+        <Route path={`${path}/account`} component={AccountDetails} />
+        <Route path={`${path}/show-private-key`} component={ShowPrivateKey} />
+      </Switch>
+    </AccountManagerProvider>
   );
 };
 
