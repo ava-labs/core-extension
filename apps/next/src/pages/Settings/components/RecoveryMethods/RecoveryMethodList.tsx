7c14b6d741790a663ea5edbcf82057aeebdbdff0
Bohdan Struk <bohdan.struk@avalabs.org>
2025-12-09 13:01:49 -0800

    fix: clean code

--- apps/next/src/pages/Settings/components/RecoveryMethods/RecoveryMethodList.tsx
+++ apps/next/src/pages/Settings/components/RecoveryMethods/RecoveryMethodList.tsx
@@ -42,7 +42,6 @@ export const RecoveryMethodList = ({
       (card.method === 'authenticator' && hasTotpConfigured) || card.isOn,
   );
 
-  console.log('recoveryMethodCards', recoveryMethodCards);
   return (
     <>
       <Paper
