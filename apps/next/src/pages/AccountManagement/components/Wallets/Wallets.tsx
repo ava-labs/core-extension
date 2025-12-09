6f0f3869515b3077e569366fc02cd4bdebf38b81
Csaba Valyi <csaba.valyi@avalabs.org>
2025-12-09 22:25:22 +0100

    feat: exposing method to fetch balances

--- apps/next/src/pages/AccountManagement/components/Wallets/Wallets.tsx
+++ apps/next/src/pages/AccountManagement/components/Wallets/Wallets.tsx
@@ -1,11 +1,11 @@
 import { Stack, Typography } from '@avalabs/k2-alpine';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Page } from '@/components/Page';
 import {
   useWalletContext,
   useWalletTotalBalance,
-  WalletTotalBalanceProvider,
+  useWalletTotalBalanceContext,
 } from '@core/ui';
 import { BulkDeleteButtons } from './components/BulkDeleteButtons';
 import { WalletList } from './components/WalletList';
@@ -20,54 +20,57 @@ export const WalletsHomePage: FC = () => {
   useLiveBalance(POLLED_BALANCES);
   const { t } = useTranslation();
   const { walletDetails } = useWalletContext();
+  const { fetchWalletBalancesSequentially } = useWalletTotalBalanceContext();
   const { isLoading, hasErrorOccurred } = useWalletTotalBalance(
     walletDetails?.id,
   );
 
-  return (
-    <WalletTotalBalanceProvider>
-      <Page
-        title={t('My wallets')}
-        titleAction={<AddOrConnectWalletButton />}
-        description={t('An overview of your wallets and associated accounts')}
-        descriptionProps={{ color: 'text.secondary' }}
-        containerProps={{
-          gap: 2,
-        }}
-        withBackButton
-        contentProps={{
-          alignItems: 'stretch',
-          justifyContent: 'flex-start',
-          position: 'relative',
-        }}
-      >
-        <Stack gap={1.5} flexGrow={1}>
-          {!isLoading && hasErrorOccurred && (
-            <Stack
-              direction="row"
-              alignItems="center"
-              gap={1}
-              justifyContent="space-between"
-            >
-              <Stack direction="row" alignItems="center" gap={1}>
-                <Styled.ErrorIcon size={24} />
-                <Typography
-                  variant="h6"
-                  color="error"
-                  fontSize="14px"
-                  whiteSpace="nowrap"
-                  id="error-message"
-                >
-                  {t('Unable to load balances')}
-                </Typography>
-              </Stack>
-            </Stack>
-          )}
-          <WalletList />
-        </Stack>
+  useEffect(() => {
+    fetchWalletBalancesSequentially();
+  }, [fetchWalletBalancesSequentially]);
 
-        <BulkDeleteButtons />
-      </Page>
-    </WalletTotalBalanceProvider>
+  return (
+    <Page
+      title={t('My wallets')}
+      titleAction={<AddOrConnectWalletButton />}
+      description={t('An overview of your wallets and associated accounts')}
+      descriptionProps={{ color: 'text.secondary' }}
+      containerProps={{
+        gap: 2,
+      }}
+      withBackButton
+      contentProps={{
+        alignItems: 'stretch',
+        justifyContent: 'flex-start',
+        position: 'relative',
+      }}
+    >
+      <Stack gap={1.5} flexGrow={1}>
+        {!isLoading && hasErrorOccurred && (
+          <Stack
+            direction="row"
+            alignItems="center"
+            gap={1}
+            justifyContent="space-between"
+          >
+            <Stack direction="row" alignItems="center" gap={1}>
+              <Styled.ErrorIcon size={24} />
+              <Typography
+                variant="h6"
+                color="error"
+                fontSize="14px"
+                whiteSpace="nowrap"
+                id="error-message"
+              >
+                {t('Unable to load balances')}
+              </Typography>
+            </Stack>
+          </Stack>
+        )}
+        <WalletList />
+      </Stack>
+
+      <BulkDeleteButtons />
+    </Page>
   );
 };
