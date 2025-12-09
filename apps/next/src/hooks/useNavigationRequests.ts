055d7d548282ecb567e89b9902fb0675984c6146
Gergely Lovas <gergely.lovas@avalabs.org>
2025-12-09 11:11:34 +0100

    feat: add support for search for already open pages

--- apps/next/src/hooks/useNavigationRequests.ts
+++ apps/next/src/hooks/useNavigationRequests.ts
@@ -19,9 +19,8 @@ export const useNavigationRequests = () => {
     const eventsSubscription = events()
       .pipe(filter(isNavigationHistoryRequestEvent))
       .subscribe((event) => {
-        console.log('event', event);
-        if (history.location.pathname !== event.value.path) {
-          history.push(event.value.path);
+        if (history.location.pathname !== event.value.pathname) {
+          history.push(event.value);
         }
       });
 
