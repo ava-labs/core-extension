19b405c665b6dda5eb5815bac4573a86864b6637
Ulises Garcia <ulises.garcia@avalabs.org>
2025-12-12 08:12:15 -0600

    fix(next-gen): blockaid screens

--- apps/next/src/pages/Approve/components/warnings/MaliciousTxOverlay.tsx
+++ apps/next/src/pages/Approve/components/warnings/MaliciousTxOverlay.tsx
@@ -1,12 +1,12 @@
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Alert } from '@avalabs/vm-module-types';
-import { MdOutlineRemoveModerator } from 'react-icons/md';
 import { Stack, Box, Typography, Button } from '@avalabs/k2-alpine';
 
 import { SlideUpDialog } from '@/components/Dialog';
 
 import { Drawer } from '../ActionDrawer';
+import { ModeratorIcon } from '@/components/ModeratorIcon';
 
 type MaliciousTxOverlayProps = {
   open: boolean;
@@ -38,9 +38,9 @@ export const MaliciousTxOverlay = ({
         gap={2}
       >
         <Box width={48} height={48}>
-          <MdOutlineRemoveModerator size={48} />
+          <ModeratorIcon color="error.main" size={48} />
         </Box>
-        <Typography variant="h4" fontWeight={700}>
+        <Typography variant="h4" fontWeight={700} maxWidth={160}>
           {alert.details.title}
         </Typography>
         <Typography variant="body1" color="text.primary">
