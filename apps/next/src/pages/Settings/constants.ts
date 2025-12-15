8e5ec0f9dbf9d49975ee6c663047c6352ce48a01
Csaba Valyi <csaba.valyi@avalabs.org>
2025-12-15 23:14:34 +0100

    feat: putting new currencies behind feature flag

--- apps/next/src/pages/Settings/constants.ts
+++ apps/next/src/pages/Settings/constants.ts
@@ -3,60 +3,68 @@ import { Currency } from './types';
 
 export const MIN_PASSWORD_LENGTH = 8;
 
-export const currencies: Currency[] = [
-  {
-    symbol: CURRENCIES.USD,
-    label: 'United States Dollar',
-    countryCode: 'us',
-  },
-  {
-    symbol: CURRENCIES.EUR,
-    label: 'Euro',
-    countryCode: 'eu',
-  },
-  {
-    symbol: CURRENCIES.GBP,
-    label: 'Pound Sterling',
-    countryCode: 'gb',
-  },
-  {
-    symbol: CURRENCIES.AUD,
-    label: 'Australian Dollar',
-    countryCode: 'au',
-  },
-  {
-    symbol: CURRENCIES.CAD,
-    label: 'Canadian Dollar',
-    countryCode: 'ca',
-  },
-  {
-    symbol: CURRENCIES.CHF,
-    label: 'Swiss Franc',
-    countryCode: 'ch',
-  },
-  {
-    symbol: CURRENCIES.HKD,
-    label: 'Hong Kong Dollar',
-    countryCode: 'hk',
-  },
-  {
-    symbol: CURRENCIES.CLP,
-    label: 'Chilean Peso',
-    countryCode: 'cl',
-  },
-  {
-    symbol: CURRENCIES.CZK,
-    label: 'Czech Crown',
-    countryCode: 'cz',
-  },
-  {
-    symbol: CURRENCIES.DKK,
-    label: 'Danish Krone',
-    countryCode: 'dk',
-  },
-  {
-    symbol: CURRENCIES.HUF,
-    label: 'Hungarian Forint',
-    countryCode: 'hu',
-  },
-];
+// TODO: once the feature flag is removed this can be turned back into a simple array
+export const getCurrencies = (
+  isBalanceServiceIntegrationOn: boolean,
+): Currency[] =>
+  [
+    {
+      symbol: CURRENCIES.USD,
+      label: 'United States Dollar',
+      countryCode: 'us',
+    },
+    {
+      symbol: CURRENCIES.EUR,
+      label: 'Euro',
+      countryCode: 'eu',
+    },
+    {
+      symbol: CURRENCIES.GBP,
+      label: 'Pound Sterling',
+      countryCode: 'gb',
+    },
+    {
+      symbol: CURRENCIES.AUD,
+      label: 'Australian Dollar',
+      countryCode: 'au',
+    },
+    {
+      symbol: CURRENCIES.CAD,
+      label: 'Canadian Dollar',
+      countryCode: 'ca',
+    },
+    {
+      symbol: CURRENCIES.CHF,
+      label: 'Swiss Franc',
+      countryCode: 'ch',
+    },
+    {
+      symbol: CURRENCIES.HKD,
+      label: 'Hong Kong Dollar',
+      countryCode: 'hk',
+    },
+    ...(isBalanceServiceIntegrationOn
+      ? [
+          {
+            symbol: CURRENCIES.CLP,
+            label: 'Chilean Peso',
+            countryCode: 'cl',
+          },
+          {
+            symbol: CURRENCIES.CZK,
+            label: 'Czech Crown',
+            countryCode: 'cz',
+          },
+          {
+            symbol: CURRENCIES.DKK,
+            label: 'Danish Krone',
+            countryCode: 'dk',
+          },
+          {
+            symbol: CURRENCIES.HUF,
+            label: 'Hungarian Forint',
+            countryCode: 'hu',
+          },
+        ]
+      : []),
+  ].flat();
