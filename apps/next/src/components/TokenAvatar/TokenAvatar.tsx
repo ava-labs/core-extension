496e3e3d20ed3c06e6d2ef25c35b4d9fd436d981
Viktor Vasas <viktor.vasas@avalabs.org>
2025-12-17 17:08:16 +0100

    feat: add fallback images

--- apps/next/src/components/TokenAvatar/TokenAvatar.tsx
+++ apps/next/src/components/TokenAvatar/TokenAvatar.tsx
@@ -1,9 +1,11 @@
-import { Badge, BadgeProps, SxProps } from '@avalabs/k2-alpine';
+import { Badge, BadgeProps, SxProps, useTheme } from '@avalabs/k2-alpine';
 
 import { FungibleTokenBalance } from '@core/types';
 
 import { ChainBadge } from '../ChainBadge';
 import { SizedAvatar } from '../SizedAvatar';
+import GenericTokenDark from '@/images/tokens/GenericTokenDark.svg';
+import GenericTokenLight from '@/images/tokens/GenericTokenLight.svg';
 
 type TokenAvatarProps = {
   size: number;
@@ -22,17 +24,33 @@ export const TokenAvatar = ({
   size = 32,
   badgeSize = 18,
   badgeSx,
-}: TokenAvatarProps) => (
-  <Badge
-    {...defaultBadgeProps}
-    badgeContent={
-      <ChainBadge
-        coreChainId={token.coreChainId}
-        size={badgeSize}
-        sx={badgeSx}
-      />
-    }
-  >
-    <SizedAvatar src={token.logoUri} alt={token.symbol} size={size} />
-  </Badge>
-);
+}: TokenAvatarProps) => {
+  const theme = useTheme();
+  const isLightMode = theme.palette.mode === 'light';
+  const fallbackImage = isLightMode ? GenericTokenLight : GenericTokenDark;
+  const avatarSrc = token.logoUri || fallbackImage;
+
+  return (
+    <Badge
+      {...defaultBadgeProps}
+      badgeContent={
+        <ChainBadge
+          coreChainId={token.coreChainId}
+          size={badgeSize}
+          sx={badgeSx}
+        />
+      }
+    >
+      <SizedAvatar src={avatarSrc} alt={token.symbol} size={size}>
+        <img
+          src={fallbackImage}
+          alt={token.symbol}
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+        />
+      </SizedAvatar>
+    </Badge>
+  );
+};
