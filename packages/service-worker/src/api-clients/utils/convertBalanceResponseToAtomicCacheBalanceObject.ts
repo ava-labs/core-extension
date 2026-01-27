372c7ec8709ac7eff7377546a611b8287c8e934c
Bartosz Nowak <bartosz.nowak@avalabs.org>
2026-01-27 17:18:12 +0100

    refactor(api-clients): remove acc split antipattern CP-13115

--- packages/service-worker/src/api-clients/utils/convertBalanceResponseToAtomicCacheBalanceObject.ts
+++ packages/service-worker/src/api-clients/utils/convertBalanceResponseToAtomicCacheBalanceObject.ts
@@ -9,73 +9,62 @@ import {
   isPvmGetBalancesResponse,
 } from './type-guards';
 
+const getChainId = (balanceResponse: BalanceResponse): number => {
+  try {
+    return caipToChainId(balanceResponse.caip2Id);
+  } catch (error) {
+    // if we are throwing error because of CoreEth, we are ignoring it
+    if (
+      !(error instanceof Error) ||
+      !error.message.includes(CORE_ETH_CAIP2ID)
+    ) {
+      throw error;
+    }
+
+    return 0;
+  }
+};
+
 export const convertBalanceResponseToAtomicCacheBalanceObject = (
   balanceResponses: BalanceResponse[],
 ): AtomicBalances => {
   return balanceResponses.reduce<AtomicBalances>(
     (accumulator, balanceResponse) => {
-      let chainId: number = 0;
-      try {
-        chainId = caipToChainId(balanceResponse.caip2Id);
-      } catch (error) {
-        // if we are throwing error because of CoreEth, we are ignoring it
-        if (
-          !(error instanceof Error) ||
-          !error.message.includes(CORE_ETH_CAIP2ID)
-        ) {
-          throw error;
-        }
-      }
+      const chainId: number = getChainId(balanceResponse);
 
       if (isPvmGetBalancesResponse(balanceResponse) && chainId !== 0) {
         // the id in the response is the walletId we passed in with the addressDetails. For X/P that should be the account address
         const accountKey = `P-${balanceResponse.id}`;
-        const mergedAccountObject = merge(
-          accumulator[chainId]?.[accountKey] ?? {},
+        accumulator[chainId] ??= {};
+        accumulator[chainId][accountKey] = merge(
+          accumulator[chainId][accountKey] ?? {},
           balanceResponse.balances.categories,
+          { nativeTokenBalance: balanceResponse.balances.nativeTokenBalance },
         );
-        return {
-          ...accumulator,
-          [chainId]: {
-            ...(accumulator[chainId] ?? {}),
-            [accountKey]: {
-              ...mergedAccountObject,
-              nativeTokenBalance: balanceResponse.balances.nativeTokenBalance,
-            },
-          },
-        };
+        return accumulator;
       }
 
       if (isAvmGetBalancesResponse(balanceResponse) && chainId !== 0) {
         // the id in the response is the walletId we passed in with the addressDetails. For X/P that should be the account address
         const accountKey = `X-${balanceResponse.id}`;
-        const mergedAccountObject = merge(
-          accumulator[chainId]?.[accountKey] ?? {},
+        accumulator[chainId] ??= {};
+        accumulator[chainId][accountKey] = merge(
+          accumulator[chainId][accountKey] ?? {},
           balanceResponse.balances.categories,
+          { nativeTokenBalance: balanceResponse.balances.nativeTokenBalance },
         );
-        return {
-          ...accumulator,
-          [chainId]: {
-            ...(accumulator[chainId] ?? {}),
-            [accountKey]: mergedAccountObject,
-          },
-        };
+        return accumulator;
       }
 
       if (isCorethGetBalancesResponse(balanceResponse) && chainId === 0) {
         const mainNetC = AVALANCHE_BLOCKCHAIN_IDS.MAINNET_C;
         const accountKey = `C-${balanceResponse.id}`;
-        const mergedAccountObject = merge(
-          accumulator[mainNetC]?.[accountKey] ?? {},
+        accumulator[mainNetC] ??= {};
+        accumulator[mainNetC][accountKey] = merge(
+          accumulator[mainNetC][accountKey] ?? {},
           balanceResponse.balances.categories,
         );
-        return {
-          ...accumulator,
-          [mainNetC]: {
-            ...(accumulator[mainNetC] ?? {}),
-            [accountKey]: mergedAccountObject,
-          },
-        };
+        return accumulator;
       }
 
       return accumulator;
--- packages/service-worker/src/api-clients/utils/convertBalanceResponsesToCacheBalanceObject.ts
+++ packages/service-worker/src/api-clients/utils/convertBalanceResponsesToCacheBalanceObject.ts
@@ -1,3 +1,4 @@
+import { TokenWithBalance } from '@avalabs/vm-module-types';
 import { AVALANCHE_BLOCKCHAIN_IDS, caipToChainId } from '@core/common';
 import { Balances } from '@core/types';
 import { CORE_ETH_CAIP2ID } from '~/api-clients/constants';
@@ -18,153 +19,118 @@ import {
   isSvmGetBalancesResponse,
 } from './type-guards';
 
+const getChainId = (balanceResponse: BalanceResponse): number => {
+  try {
+    return caipToChainId(balanceResponse.caip2Id);
+  } catch (error) {
+    // if we are throwing error because of CoreEth, we are ignoring it
+    if (
+      !(error instanceof Error) ||
+      !error.message.includes(CORE_ETH_CAIP2ID)
+    ) {
+      throw error;
+    }
+
+    return 0;
+  }
+};
+
 export const convertBalanceResponsesToCacheBalanceObject = (
   balanceResponses: BalanceResponse[],
 ): Balances => {
   return balanceResponses.reduce<Balances>((accumulator, balanceResponse) => {
-    let chainId: number = 0;
-    try {
-      chainId = caipToChainId(balanceResponse.caip2Id);
-    } catch (error) {
-      // if we are throwing error because of CoreEth, we are ignoring it
-      if (
-        !(error instanceof Error) ||
-        !error.message.includes(CORE_ETH_CAIP2ID)
-      ) {
-        throw error;
-      }
-    }
+    const nativeSymbol = balanceResponse.balances.nativeTokenBalance.symbol;
+    const chainId: number = getChainId(balanceResponse);
 
     if (isEvmGetBalancesResponse(balanceResponse) && chainId !== 0) {
       const tokenBalanceMapper = mapErc20TokenBalance(chainId);
+      const accountKey = balanceResponse.id;
+      accumulator[chainId] ??= {};
+      accumulator[chainId][accountKey] ??= {};
+      accumulator[chainId][accountKey][nativeSymbol] = mapNativeTokenBalance(
+        balanceResponse.balances.nativeTokenBalance,
+      );
 
-      return {
-        ...accumulator,
-        [chainId]: {
-          ...(accumulator[chainId] ?? {}),
-          [balanceResponse.id]: {
-            ...(accumulator[chainId]?.[balanceResponse.id] ?? {}),
-            [balanceResponse.balances.nativeTokenBalance.symbol]:
-              mapNativeTokenBalance(
-                balanceResponse.balances.nativeTokenBalance,
-              ),
-            ...balanceResponse.balances.erc20TokenBalances.reduce(
-              (acc, tokenBalance) => {
-                const mappedTokenBalance = tokenBalanceMapper(tokenBalance);
+      balanceResponse.balances.erc20TokenBalances.reduce(
+        (acc, tokenBalance) => {
+          const mappedTokenBalance = tokenBalanceMapper(tokenBalance);
 
-                if (!mappedTokenBalance) {
-                  return acc;
-                }
-                return {
-                  ...acc,
-                  [tokenBalance.address]: mappedTokenBalance,
-                };
-              },
-              {},
-            ),
-          },
+          if (mappedTokenBalance) {
+            acc[tokenBalance.address] = mappedTokenBalance;
+          }
+
+          return acc;
         },
-      };
+        accumulator[chainId][accountKey],
+      );
+
+      return accumulator;
     }
 
     if (isPvmGetBalancesResponse(balanceResponse) && chainId !== 0) {
       // the id in the response is the walletId we passed in with the addressDetails. For X/P that should be the account address
       const accountKey = `P-${balanceResponse.id}`;
-      return {
-        ...accumulator,
-        [chainId]: {
-          ...(accumulator[chainId] ?? {}),
-          [accountKey]: {
-            ...(accumulator[chainId]?.[accountKey] ?? {}),
-            [balanceResponse.balances.nativeTokenBalance.symbol]:
-              mapPvmTokenBalance(balanceResponse),
-          },
-        },
-      };
+      accumulator[chainId] ??= {};
+      accumulator[chainId][accountKey] ??= {};
+      accumulator[chainId][accountKey][nativeSymbol] =
+        mapPvmTokenBalance(balanceResponse);
+      return accumulator;
     }
 
     if (isAvmGetBalancesResponse(balanceResponse) && chainId !== 0) {
       // the id in the response is the walletId we passed in with the addressDetails. For X/P that should be the account address
       const accountKey = `X-${balanceResponse.id}`;
-      return {
-        ...accumulator,
-        [chainId]: {
-          ...(accumulator[chainId] ?? {}),
-          [accountKey]: {
-            ...(accumulator[chainId]?.[accountKey] ?? {}),
-            [balanceResponse.balances.nativeTokenBalance.symbol]:
-              mapAvmTokenBalance(balanceResponse),
-          },
-        },
-      };
+      accumulator[chainId] ??= {};
+      accumulator[chainId][accountKey] ??= {};
+      accumulator[chainId][accountKey][nativeSymbol] =
+        mapAvmTokenBalance(balanceResponse);
+      return accumulator;
     }
 
     if (isBtcGetBalancesResponse(balanceResponse) && chainId !== 0) {
-      return {
-        ...accumulator,
-        [chainId]: {
-          ...(accumulator[chainId] ?? {}),
-          [balanceResponse.id]: {
-            ...(accumulator[chainId]?.[balanceResponse.id] ?? {}),
-            [balanceResponse.balances.nativeTokenBalance.symbol]:
-              mapNativeTokenBalance(
-                balanceResponse.balances.nativeTokenBalance,
-              ),
-          },
-        },
-      };
+      const accountKey = balanceResponse.id;
+      accumulator[chainId] ??= {};
+      accumulator[chainId][accountKey] ??= {};
+      accumulator[chainId][accountKey][nativeSymbol] = mapNativeTokenBalance(
+        balanceResponse.balances.nativeTokenBalance,
+      );
+      return accumulator;
     }
 
     if (isSvmGetBalancesResponse(balanceResponse) && chainId !== 0) {
-      return {
-        ...accumulator,
-        [chainId]: {
-          ...(accumulator[chainId] ?? {}),
-          [balanceResponse.id]: {
-            ...(accumulator[chainId]?.[balanceResponse.id] ?? {}),
-            [balanceResponse.balances.nativeTokenBalance.symbol]:
-              mapNativeTokenBalance(
-                balanceResponse.balances.nativeTokenBalance,
-              ),
-            ...balanceResponse.balances.splTokenBalances.reduce(
-              (acc, tokenBalance) => {
-                const mappedTokenBalance = mapSplTokenBalance(tokenBalance);
+      const accountKey = balanceResponse.id;
+      accumulator[chainId] ??= {};
+      accumulator[chainId][accountKey] ??= {};
+      accumulator[chainId][accountKey][nativeSymbol] = mapNativeTokenBalance(
+        balanceResponse.balances.nativeTokenBalance,
+      );
+      balanceResponse.balances.splTokenBalances.reduce((acc, tokenBalance) => {
+        const mappedTokenBalance = mapSplTokenBalance(tokenBalance);
 
-                if (!mappedTokenBalance) {
-                  return acc;
-                }
-                return {
-                  ...acc,
-                  [tokenBalance.address]: mappedTokenBalance,
-                };
-              },
-              {},
-            ),
-          },
-        },
-      };
+        if (mappedTokenBalance) {
+          acc[tokenBalance.address] = mappedTokenBalance;
+        }
+
+        return acc;
+      }, accumulator[chainId][accountKey]);
+
+      return accumulator;
     }
 
     if (isCorethGetBalancesResponse(balanceResponse) && chainId === 0) {
       const mainNetC = AVALANCHE_BLOCKCHAIN_IDS.MAINNET_C;
       const accountKey = `C-${balanceResponse.id}`;
-      return {
-        ...accumulator,
-        [mainNetC]: {
-          ...(accumulator[mainNetC] ?? {}),
-          [accountKey]: {
-            ...(accumulator[mainNetC]?.[accountKey] ?? {}),
-            [balanceResponse.balances.nativeTokenBalance.symbol]:
-              mapNativeTokenBalance(
-                balanceResponse.balances.nativeTokenBalance,
-              ),
-            categories: {
-              ...(accumulator[mainNetC]?.[accountKey]?.categories ?? {}),
-              ...balanceResponse.balances.categories,
-            },
-          },
-        },
-      } as Balances;
+      accumulator[mainNetC] ??= {};
+      accumulator[mainNetC][accountKey] ??= {};
+      accumulator[mainNetC][accountKey][nativeSymbol] = mapNativeTokenBalance(
+        balanceResponse.balances.nativeTokenBalance,
+      );
+      accumulator[mainNetC][accountKey].categories = {
+        ...accumulator[mainNetC][accountKey].categories,
+        ...(balanceResponse.balances.categories as unknown as TokenWithBalance),
+      };
+
+      return accumulator;
     }
 
     return accumulator;
