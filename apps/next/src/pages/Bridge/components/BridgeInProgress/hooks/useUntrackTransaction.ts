42085fc6188c348c3c99a39176b472e347bea616
Michał Leszczyk <michal.leszczyk@avalabs.org>
2026-01-30 13:25:45 +0100

    chore: cleanup avalanche btc<>btc.b bridge leftovers

--- apps/next/src/pages/Bridge/components/BridgeInProgress/hooks/useUntrackTransaction.ts
+++ apps/next/src/pages/Bridge/components/BridgeInProgress/hooks/useUntrackTransaction.ts
@@ -1,12 +1,12 @@
 import { ExtensionRequest } from '@core/types';
-import { BridgeContext, useConnectionContext } from '@core/ui';
+import { useConnectionContext } from '@core/ui';
 import { useCallback } from 'react';
 import { BridgeRemoveTransactionHandler } from '~/services/bridge/handlers/removeBridgeTransaction';
 
 export const useUntrackTransaction = () => {
   const { request } = useConnectionContext();
-  return useCallback<BridgeContext['removeBridgeTransaction']>(
-    async (txHash) => {
+  return useCallback(
+    async (txHash: string) => {
       await request<BridgeRemoveTransactionHandler>({
         method: ExtensionRequest.BRIDGE_TRANSACTION_REMOVE,
         params: [txHash],
