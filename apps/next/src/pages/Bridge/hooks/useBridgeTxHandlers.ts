927f76ee6d020cd5263e89ece84f9f812979b55e
Fumiko Richards <6504659+frichards@users.noreply.github.com>
2026-02-16 09:31:23 -0700

    fix(analytics): add analytics (#773)

--- apps/next/src/pages/Bridge/hooks/useBridgeTxHandlers.ts
+++ apps/next/src/pages/Bridge/hooks/useBridgeTxHandlers.ts
@@ -3,7 +3,11 @@ import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useBridgeQuery } from '../contexts';
 import { toast } from '@avalabs/k2-alpine';
-import { useErrorMessage } from '@core/ui';
+import {
+  useAccountsContext,
+  useAnalyticsContext,
+  useErrorMessage,
+} from '@core/ui';
 
 export type TransferResult = Awaited<
   ReturnType<typeof handleTxOutcome<string>>
@@ -13,8 +17,16 @@ const TOAST_ID = 'bridge-result';
 export const useBridgeTxHandlers = () => {
   const [isAddressBlocked, setIsAddressBlocked] = useState(false);
   const getTranslatedError = useErrorMessage();
+  const { captureEncrypted } = useAnalyticsContext();
+  const {
+    accounts: { active: activeAccount },
+  } = useAccountsContext();
 
-  const { updateQuery } = useBridgeQuery();
+  const {
+    updateQuery,
+    sourceNetwork: sourceNetworkId,
+    targetNetwork: targetNetworkId,
+  } = useBridgeQuery();
   const { t } = useTranslation();
 
   const onBridgeError = useCallback(
@@ -34,9 +46,21 @@ export const useBridgeTxHandlers = () => {
 
   const onSuccess = useCallback(
     (result: Extract<TransferResult, { hasError: false }>) => {
+      captureEncrypted('BridgeTransferRequestSucceeded', {
+        address: activeAccount?.addressC ?? '',
+        txHash: result.result,
+        sourceBlockchain: sourceNetworkId,
+        targetBlockchain: targetNetworkId,
+      });
       updateQuery({ transactionId: result.result });
     },
-    [updateQuery],
+    [
+      activeAccount?.addressC,
+      captureEncrypted,
+      sourceNetworkId,
+      targetNetworkId,
+      updateQuery,
+    ],
   );
 
   const onRejected = useCallback(() => {
