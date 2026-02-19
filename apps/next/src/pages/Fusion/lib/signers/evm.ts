d11d47480714ab37d0f87de209567df3d0fb241a
Michał Leszczyk <michal.leszczyk@avalabs.org>
2026-02-19 09:11:41 +0100

    feat(fusion): allow fetching quotes for cross chain swaps (#762)

--- apps/next/src/pages/Fusion/lib/signers/evm.ts
+++ apps/next/src/pages/Fusion/lib/signers/evm.ts
@@ -1,6 +1,7 @@
+import { hex, utf8 } from '@scure/base';
 import { TFunction } from 'react-i18next';
 import { RpcMethod } from '@avalabs/vm-module-types';
-import { EvmSigner } from '@avalabs/unified-asset-transfer';
+import { EvmSignerWithMessage } from '@avalabs/unified-asset-transfer';
 
 import { assert, chainIdToCaip } from '@core/common';
 import { RequestHandlerType, UnifiedBridgeError } from '@core/types';
@@ -8,7 +9,7 @@ import { RequestHandlerType, UnifiedBridgeError } from '@core/types';
 export function getEVMSigner(
   request: RequestHandlerType,
   _t: TFunction,
-): EvmSigner {
+): EvmSignerWithMessage {
   return {
     sign: async ({ from, data, to, value, chainId }) => {
       assert(to, UnifiedBridgeError.InvalidTxPayload);
@@ -39,7 +40,35 @@ export function getEVMSigner(
 
         return result as `0x${string}`;
       } catch (err) {
-        console.error(err);
+        console.error(`[fusion::evmSigner.sign]`, err);
+        throw err;
+      }
+    },
+
+    signMessage: async (data: {
+      message: string;
+      address: `0x${string}`;
+      chainId: number;
+    }) => {
+      const { message, address, chainId } = data;
+
+      assert(message, UnifiedBridgeError.InvalidTxPayload);
+      assert(address, UnifiedBridgeError.InvalidTxPayload);
+
+      try {
+        const result = await request(
+          {
+            method: RpcMethod.PERSONAL_SIGN,
+            params: [`0x${hex.encode(utf8.decode(message))}`, address],
+          },
+          {
+            scope: `eip155:${chainId}`,
+          },
+        );
+
+        return result as `0x${string}`;
+      } catch (err) {
+        console.error(`[fusion::evmSigner.signMessage]`, err);
         throw err;
       }
     },
--- apps/next/src/pages/Portfolio/components/PortfolioHome/components/PortolioDetails/components/PortfolioActionButtons.tsx
+++ apps/next/src/pages/Portfolio/components/PortfolioHome/components/PortolioDetails/components/PortfolioActionButtons.tsx
@@ -80,7 +80,7 @@ export const PortfolioActionButtons = ({
         gap: 1,
       }}
     >
-      {isFlagEnabled(FeatureGates.FUSION_PROJECT) ? (
+      {isFlagEnabled(FeatureGates.FUSION_FEATURE) ? (
         <Fade in timeout={getDelay()} easing="ease-out">
           <OfflineTooltip placement="top">
             <SquareButton
