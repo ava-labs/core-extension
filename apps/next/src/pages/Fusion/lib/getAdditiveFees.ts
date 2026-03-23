a0a7cea7a1aaacb80b4c78885445094e9992c233
Michał Leszczyk <michal.leszczyk@avalabs.org>
2026-03-23 14:43:10 +0100

    fix(fusion): stable reference for empty fees

--- apps/next/src/pages/Fusion/lib/getAdditiveFees.ts
+++ apps/next/src/pages/Fusion/lib/getAdditiveFees.ts
@@ -1,10 +1,15 @@
-import type { Quote } from '@avalabs/fusion-sdk';
+import type { Quote, QuoteFee } from '@avalabs/fusion-sdk';
 
-// Prevents unnecessary re-renders upon reference change.
 const EMPTY_ARRAY = [];
 
-export const getAdditiveFees = (quote: Quote | null) => {
-  return quote
-    ? quote.fees.filter((fee) => fee.fundingModel === 'additive')
-    : EMPTY_ARRAY;
+export const getAdditiveFees = (quote: Quote | null): QuoteFee[] => {
+  if (!quote) {
+    return EMPTY_ARRAY;
+  }
+
+  const additiveFees = quote.fees.filter(
+    (fee) => fee.fundingModel === 'additive',
+  );
+
+  return additiveFees.length === 0 ? EMPTY_ARRAY : additiveFees;
 };
