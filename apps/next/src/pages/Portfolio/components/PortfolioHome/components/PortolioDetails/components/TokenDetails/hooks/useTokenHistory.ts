50742042b4253b6384708018df31a369fc02e91f
Fumiko <fumikom@gmail.com>
2026-03-27 17:01:43 -0600

    fix: token activity loading state

--- apps/next/src/pages/Portfolio/components/PortfolioHome/components/PortolioDetails/components/TokenDetails/hooks/useTokenHistory.ts
+++ apps/next/src/pages/Portfolio/components/PortfolioHome/components/PortolioDetails/components/TokenDetails/hooks/useTokenHistory.ts
@@ -1,7 +1,6 @@
 import { FungibleTokenBalance, Network, TxHistoryItem } from '@core/types';
 import { useMemo } from 'react';
 import { useAccountHistory } from '../../ActivityTab/hooks';
-import { NetworkVMType } from '@avalabs/vm-module-types';
 import { useToken } from './useToken';
 
 type Props = {
@@ -9,27 +8,28 @@ type Props = {
   tokenAddress: string;
 };
 
-const tokenMatcher = (
-  tx: TxHistoryItem<NetworkVMType>,
-  token: FungibleTokenBalance,
-) => {
+const tokenMatcher = (tx: TxHistoryItem, token: FungibleTokenBalance) => {
   const matchedTokens = tx.tokens.filter((txToken) => {
-    return txToken.symbol.toLowerCase() === token.symbol.toLowerCase();
+    return txToken.symbol?.toLowerCase() === token.symbol.toLowerCase();
   });
 
   return matchedTokens.length > 0;
 };
 
-export const useTokenHistory = ({ networkId, tokenAddress }: Props) => {
+export const useTokenHistory = ({
+  networkId,
+  tokenAddress,
+}: Props): TxHistoryItem[] | null => {
   const transactionHistory = useAccountHistory(networkId);
   const token = useToken(tokenAddress, networkId);
 
-  const filteredTransactionHistory = useMemo(() => {
+  return useMemo(() => {
+    if (transactionHistory === null) {
+      return null;
+    }
     if (!token) {
       return [];
     }
-    return transactionHistory?.filter((tx) => tokenMatcher(tx, token)) ?? [];
+    return transactionHistory.filter((tx) => tokenMatcher(tx, token));
   }, [transactionHistory, token]);
-
-  return filteredTransactionHistory;
 };
