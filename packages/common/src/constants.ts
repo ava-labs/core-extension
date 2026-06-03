bdfa3cd30d1c636e3ed97c5db784971a18feff2d
bferenc <117105626+bferenc@users.noreply.github.com>
2026-06-03 16:21:50 +0200

    add playground app to core domains (#944)

--- packages/common/src/constants.ts
+++ packages/common/src/constants.ts
@@ -35,10 +35,11 @@ const KNOWN_AVACLOUD_DOMAINS = [
   'launchpad.avacloud.io',
 ];
 
-const PLAYGROUD_APP = 'ava-labs.github.io';
+const PLAYGROUND_APP = 'ava-labs.github.io' as const;
 
 export const KNOWN_CORE_DOMAINS = [
   CORE_WEB_DOMAIN,
+  PLAYGROUND_APP,
   ...CORE_WEB_STAGING_DOMAINS,
   ...DAPP_DEV_DOMAINS,
 ] as const;
@@ -48,7 +49,6 @@ export const WHITELISTED_DOMAINS = [
   ...KNOWN_CORE_DOMAINS,
   ...KNOWN_AVACLOUD_DOMAINS,
   ...DAPP_DEV_DOMAINS,
-  PLAYGROUD_APP,
 ];
 
 export const SYNCED_DOMAINS = isProductionBuild()
