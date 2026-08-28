a6882e916eec0a7e2b6969f6b26e213cdcea67bf
Gergely Lovas <gergely.lovas@avalabs.org>
2026-08-28 21:43:04 +0200

    chore: tweak deprecation message

--- apps/next/src/components/KeystoneDeprecationBanner.tsx
+++ apps/next/src/components/KeystoneDeprecationBanner.tsx
@@ -1,11 +1,10 @@
 import { FC } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans } from 'react-i18next';
 import { MdError } from 'react-icons/md';
 import { Stack, Typography, useTheme } from '@avalabs/k2-alpine';
 import { useIsUsingKeystoneWallet } from '@core/ui';
 
 export const KeystoneDeprecationBanner: FC = () => {
-  const { t } = useTranslation();
   const theme = useTheme();
   const isUsingKeystoneWallet = useIsUsingKeystoneWallet();
 
@@ -16,8 +15,8 @@ export const KeystoneDeprecationBanner: FC = () => {
   return (
     <Stack
       direction="row"
-      alignItems="flex-start"
-      gap={0.5}
+      alignItems="center"
+      gap={1}
       data-testid="keystone-deprecation-banner"
     >
       <MdError
@@ -26,9 +25,7 @@ export const KeystoneDeprecationBanner: FC = () => {
         style={{ flexShrink: 0 }}
       />
       <Typography variant="subtitle4" color="error">
-        {t(
-          'Support for Keystone wallets was deprecated on September 1st, 2026',
-        )}
+        <Trans i18nKey="Support for Keystone wallets was<br/>deprecated on September 1st, 2026" />
       </Typography>
     </Stack>
   );
